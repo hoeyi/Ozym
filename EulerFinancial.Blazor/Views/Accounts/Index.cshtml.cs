2522431d925e47d1d9a96a66e137e43149dd7926
hoeyi <48498580+hoeyi@users.noreply.github.com>
2021-11-07 13:39:59 -0500

    fix: add back example Razor page views

---
+++ EulerFinancial.Blazor/Views/Accounts/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using EulerFinancial.Context;
+using EulerFinancial.Model;
+
+namespace EulerFinancial.Blazor.Views.Accounts
+{
+    public class IndexModel : PageModel
+    {
+        private readonly EulerFinancial.Context.EulerFinancialContext _context;
+
+        public IndexModel(EulerFinancial.Context.EulerFinancialContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Account> Account { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Account = await _context.Accounts
+                .Include(a => a.AccountCustodian)
+                .Include(a => a.AccountNavigation).ToListAsync();
+        }
+    }
+}
