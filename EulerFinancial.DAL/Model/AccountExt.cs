82b4d4a79c499cce91127fb3e523b8e68ad838a1
hoeyi <48498580+hoeyi@users.noreply.github.com>
2021-11-27 13:36:41 -0500

    feat: add unmapped Account properties

--- EulerFinancial.DAL/Model/AccountExt.cs
+++ EulerFinancial.DAL/Model/AccountExt.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EulerFinancial.Model
 {
     public partial class Account
     {
+        [NotMapped]
         public DateTime StartDate
         {
             get{ return AccountNavigation?.StartDate ?? default; }
+        }
+
+        [NotMapped]
+        public DateTime? CloseDate
+        {
+            get{ return AccountNavigation?.CloseDate; }
+        }
+
+        [NotMapped]
+        public string DisplayName
+        {
+            get{ return AccountNavigation?.ObjectDipslayName; }
+        }
+
+        [NotMapped]
+        public string Description
+        {
+            get{ return AccountNavigation?.ObjectDescription; }
         }
     }
 }
