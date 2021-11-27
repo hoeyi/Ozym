8cd835b7ea6b07f4f6c0d637f3b79aa0501d5285
hoeyi <48498580+hoeyi@users.noreply.github.com>
2021-11-27 12:10:51 -0500

    refactor: split QueryParameter class into separate file

--- EulerFinancial.DAL/Expressions/IQueryParameter.cs
+++ EulerFinancial.DAL/Expressions/IQueryParameter.cs
@@ -28,28 +28,5 @@ namespace EulerFinancial.Expressions
         TParam Value { get; }
     }
 
-    public class QueryParameter<TModel, TParam> : IQueryParameter<TModel, TParam>
-    {
-        public QueryParameter(string memberName, ComparisonOperator @operator, TParam paramValue)
-        {
-            if(SearchObjectType?.GetProperty(memberName) is null)
-            {
-                var invalidMemberMessage = string.Format(Resources.ExceptionString.Search_ParameterNotValidForType,
-                    memberName ?? Resources.Adjective.Empty, SearchObjectType.Name);
 
-                throw new InvalidOperationException(message: invalidMemberMessage);
-            }
-            MemberName = memberName;
-            Operator = @operator;
-            Value = paramValue;
-        }
-
-        public Type SearchObjectType { get => typeof(TModel); }
-
-        public string MemberName { get; }
-
-        public ComparisonOperator Operator { get; }
-
-        public TParam Value { get; }
-    }
 }
