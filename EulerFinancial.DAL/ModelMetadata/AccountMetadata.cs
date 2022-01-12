a18856f689e14ab3088aa976a1651c0d414e4c14
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-01-12 18:47:50 -0500

    feat: add example class for custom attributes in EF-generated classes

---
+++ EulerFinancial.DAL/ModelMetadata/AccountMetadata.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EulerFinancial.Model;
+
+namespace EulerFinancial.ModelMetadata
+{
+    // Add class as an exmaple of using metadata class to add 
+    // custom attributes to EF-generated classes.
+    public class AccountMetadata
+    {
+        public string AccountNumber { get; set; }
+    }
+
+    [MetadataType(typeof(AccountMetadata))]
+    public partial class Account
+    {
+    }
+}
