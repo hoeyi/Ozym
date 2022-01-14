bd0fcd3d77ec81fc81a46d6c0daecb71cd996132
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-01-13 22:33:10 -0500

    chore: remove obsolete class and unnecessary using statements

--- EulerFinancial.DAL/Extensions/StringExtension.cs
+++ EulerFinancial.DAL/Extensions/StringExtension.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EulerFinancial.Extensions
+﻿namespace EulerFinancial.Extensions
 {
     public static class StringExtension
     {
