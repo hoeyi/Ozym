138de7e4ab6ac5d96c9dbb6bcd91171555b7297c
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-01-23 15:00:36 -0500

    docs: add in-line code documentation to AssemblyInfo helper class

--- EulerFinancial.DAL/Configuration/AssemblyInfo.cs
+++ EulerFinancial.DAL/Configuration/AssemblyInfo.cs
@@ -1,11 +1,15 @@
-﻿using System.Reflection;
-using System.IO;
+﻿using System.IO;
+using System.Reflection;
 
 namespace EulerFinancial.Configuration
 {
     public static class AssemblyInfo
     {
         #region Assembly attributes
+        /// <summary>
+        /// Gets the title of the calling assembly from the <see cref="AssemblyTitleAttribute"/>, 
+        /// else the <see cref="Assembly.FullName"/> if none exists.
+        /// </summary>
         public static string AssemblyTitle
         {
             get
@@ -15,7 +19,8 @@ namespace EulerFinancial.Configuration
                 if (attributes.Length > 0)
                 {
                     AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
-                    return string.IsNullOrEmpty(titleAttribute.Title) ? assembly.FullName : titleAttribute.Title;
+                    return string
+                        .IsNullOrEmpty(titleAttribute.Title) ? assembly.FullName : titleAttribute.Title;
                 }
                 else
                 {
@@ -23,7 +28,11 @@ namespace EulerFinancial.Configuration
                 }
             }
         }
-        
+
+        /// <summary>
+        /// Gets the version of the calling assembly as a string from the 
+        /// <see cref="AssemblyVersionAttribute"/>.
+        /// </summary>
         public static string AssemblyVersion
         {
             get
@@ -31,59 +40,86 @@ namespace EulerFinancial.Configuration
                 return Assembly.GetCallingAssembly().GetName().Version.ToString();
             }
         }
-        
+
+        /// <summary>
+        /// Gets the description of the calling assembly from the <see cref="AssemblyDescriptionAttribute"/>, 
+        /// or an empty string if none exists.
+        /// </summary>
         public static string AssemblyDescription
         {
             get
             {
-                object[] attributes = Assembly.GetCallingAssembly().GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
+                object[] attributes = Assembly
+                    .GetCallingAssembly()
+                    .GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
                 if (attributes.Length == 0)
                 {
-                    return "";
+                    return string.Empty;
                 }
                 return ((AssemblyDescriptionAttribute)attributes[0]).Description;
             }
         }
-        
+
+        /// <summary>
+        /// Gets the description of the calling assembly from the <see cref="AssemblyDescriptionAttribute"/>, 
+        /// or an empty string if none exists.
+        /// </summary>
         public static string AssemblyProduct
         {
             get
             {
-                object[] attributes = Assembly.GetCallingAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
+                object[] attributes = Assembly
+                    .GetCallingAssembly()
+                    .GetCustomAttributes(typeof(AssemblyProductAttribute), false);
                 if (attributes.Length == 0)
                 {
-                    return "";
+                    return string.Empty;
                 }
                 return ((AssemblyProductAttribute)attributes[0]).Product;
             }
         }
-        
+
+        /// <summary>
+        /// Gets the copyright of the calling assembly from the <see cref="AssemblyCopyrightAttribute"/>, 
+        /// or an empty string if none exists.
+        /// </summary>
         public static string AssemblyCopyright
         {
             get
             {
-                object[] attributes = Assembly.GetCallingAssembly().GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
+                object[] attributes = Assembly
+                    .GetCallingAssembly()
+                    .GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
                 if (attributes.Length == 0)
                 {
-                    return "";
+                    return string.Empty;
                 }
                 return ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
             }
         }
-        
+
+        /// <summary>
+        /// Gets the company of the calling assembly from the <see cref="AssemblyCompanyAttribute"/>, 
+        /// or an empty string if none exists.
+        /// </summary>
         public static string AssemblyCompany
         {
             get
             {
-                object[] attributes = Assembly.GetCallingAssembly().GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
+                object[] attributes = Assembly
+                    .GetCallingAssembly()
+                    .GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
                 if (attributes.Length == 0)
                 {
-                    return "";
+                    return string.Empty;
                 }
                 return ((AssemblyCompanyAttribute)attributes[0]).Company;
             }
         }
-        
+
+        /// <summary>
+        /// Gets the simple name for the calling assembly.
+        /// </summary>
         public static string AssemblyName
         {
             get
@@ -92,6 +128,9 @@ namespace EulerFinancial.Configuration
             }
         }
 
+        /// <summary>
+        /// Gets the full path or UNC location for the calling assembly.
+        /// </summary>
         public static string AssemblyPath
         {
             get
@@ -99,7 +138,10 @@ namespace EulerFinancial.Configuration
                 return Path.GetDirectoryName(Assembly.GetCallingAssembly().Location);
             }
         }
-        
+
+        /// <summary>
+        /// Gets the full path or UNC location for the executing assembly.
+        /// </summary>
         public static string ExecutingAssemblyPath
         {
             get
