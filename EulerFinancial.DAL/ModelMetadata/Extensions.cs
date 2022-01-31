75f434b6d9afcba8e7ded33bdfea2b259d7478ed
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-01-30 19:32:02 -0500

    Feat/UI components (#5)
    * build: add Hoeyi.Configuration.Extensions reference
    
    * build: add package reference for Hoey.Configuration.Extensions
    
    * build: fix package dependencies
    
    * build: update project for Hoeyi.Extensions.Configuration reference
    
    * fix: secure configuration usage in WebBuilder
    
    * fix: configuration implementation
    
    * test: comment out protected configuration test
    
    * build: update dependencies
    
    * fix: update configuration creation to ensure RsaKeyContainer is set
    
    * feat: add example class for custom attributes in EF-generated classes
    
    * build: update package dependency
    
    * feat: move Expressions functions to separate library
    
    Separates search functionality from main project into separate library. Updates
    references to new Ichosoft.Expressions library. Requires decorating model metadata
    with 'Searchable' attribute.
    
    * chore: remove obsolete class and unnecessary using statements
    
    * refactor: simplify project structure by spinning off features
    
    Removes Expressions and UI helper elements and replaces them with a separate
    library for model metadata: DataModel.Annotations and DataModel.Expressions.
    
    * fix: revise namespaces and clean-up test space
    
    * feat: add more metadata helper methods
    
    * fix: resolve typo in AccountObject member
    
    * chore: reorganize project structure
    
    Move metadata classes into separate library. Consolidate model string
    resource by attribute. Allows all elements of display attribute to draw from
    one location.
    
    * feat: add NounAttribute to example model metadata
    
    Add NounAttribute definition to metadata classes and remove obsolete code.
    
    * feat: update Blazor app to utilize third-party libraries for localization
    
    * build: update package dependencies to latest versions
    
    * fix: revise views to use built-in ToString methods
    
    * feat: add localization
    
    * Feat/account editor (#4)
    
    * chore: clean-up unnecessary using statements
    
    * feat: add account CRUD component and generalize model components
    
    Implement account editor feature and generalize implementation by using generic components.
    Uses generic components to define a functionality hierarchy where common component properties
    like ILogger  and IController are inherited from parents.
    
    * refactor: add reuse of methods in base classes for blazor components
    
    * feat: add create account component
    
    * feat: revise create account component and add suport for deletion
    
    * docs: add in-line code documentation to AssemblyInfo helper class
    
    * refactor: organize custom components and simplify adding new ones
    
    Simplifies adding new components by moving reusable code to ModelComponentBase. Also
    implements default behavior for page routing of models. Inheritors can override default routing.
    Reorganization is done to make generic components easier to extract into separate package.
    
    * build: upgrade to .NET 6
    
    * fix: resolve checkbox rendering bug
    
    Fix rendering issue for checkboxes caused by use of class attribute equal
    to 'form-control' in <InputCheckbox> element.
    
    * chore: remove obsolete files

---
+++ EulerFinancial.DAL/ModelMetadata/Extensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.Serialization;
+using Ichosoft.DataModel;
+using Ichosoft.DataModel.Annotations;
+
+namespace EulerFinancial.ModelMetadata
+{
+    /// <summary>
+    /// Extensions methods useful for accessing model metadata.
+    /// </summary>
+    public static class Extensions
+    {  
+        /// <summary>
+        /// Gets the <see cref="NounAttribute"/> applied to the given type.
+        /// </summary>
+        /// <param name="metadata"></param>
+        /// <param name="type">The type decorated with a noun attribute.</param>
+        /// <returns>A <see cref="NounAttribute"/> if applied to the type, else null.</returns>
+        public static NounAttribute NounFor(this IModelMetadataService metadata, Type type)
+        {
+            if (type is null)
+                return null;
+
+            return metadata?.AttributeFor<NounAttribute>(type);
+        }
+
+        /// <summary>
+        /// Returns the string value assigned to the member <see cref="EnumMemberAttribute"/>.
+        /// </summary>
+        /// <typeparam name="TEnum">The enum type.</typeparam>
+        /// <param name="enum"></param>
+        /// <returns>The string representation of this member.</returns>
+        public static string ConvertToStringCode<TEnum>(this TEnum @enum)
+            where TEnum : struct
+        {
+            var enumType = typeof(TEnum);
+            var name = Enum.GetName(enumType, @enum);
+
+            var memberAttribute = enumType.AttributeFor<EnumMemberAttribute>(name);
+
+            return memberAttribute?.Value;
+        }
+    }
+}
--- EulerFinancial.DAL/Validation/DomainValidator.cs
+++ EulerFinancial.DAL/Validation/DomainValidator.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using EulerFinancial.Resources;
 
 namespace EulerFinancial.Validation
 {
@@ -27,22 +26,10 @@ namespace EulerFinancial.Validation
             return modelValidationDelegate(model, out errors);
         }
 
-        public string GetModelGeneralValidationFailedMessage<T>(IEnumerable<string> validationErrors)
-        {
-            var nounMetadata = ResourceHelper.GetNounMetadata($"Model.{typeof(T).Name}");
-
-            string msg = string.Format("{0} {1}",
-                ResourceHelper.ToTitleCase(nounMetadata?.SingularArticle),
-                nounMetadata?.Singular ?? typeof(T).Name);
-
-            string errors = string.Join("\n", validationErrors);
-            return validationErrors?.Count() > 0 ? $"{msg}\n\r\n\r{errors}" : msg;
-        }
-
         protected virtual bool ModelIsValidDel(object model, out IList<string> errors)
         {
             ICollection<ValidationResult> results = new List<ValidationResult>();
-            ValidationContext context = new ValidationContext(model);
+            ValidationContext context = new(model);
 
             bool isValid = Validator.TryValidateObject(model, context, results);
 
