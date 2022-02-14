c2a0e151a4b5662754dfacd05a9a4b540da22919
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-02-14 18:36:34 -0500

    feat: add datagrid component

---
+++ EulerFinancial.Blazor/Components/Generic/PropertyAccessor.cs
@@ -0,0 +1,30 @@
+﻿namespace EulerFinancial.Blazor.Components.Generic
+{
+    public static class PropertyAccessor
+    {
+        public static object GetValue<TModel>(string path, object obj)
+        {
+            var type = typeof(TModel);
+
+            var pathElements = path.Split(".");
+
+            if (pathElements.Length == 1)
+            {
+                return type.GetProperty(path)?.GetValue(obj);
+            }
+            else if (pathElements.Length == 2)
+            {
+                var complexMemberInfo = type.GetProperty(pathElements[0]);
+
+                var complexMemberValue = complexMemberInfo?.GetValue(obj);
+
+                return complexMemberInfo
+                    ?.PropertyType
+                    ?.GetProperty(pathElements[1])
+                    ?.GetValue(complexMemberValue);
+            }
+            else
+                return null;
+        }
+    }
+}
