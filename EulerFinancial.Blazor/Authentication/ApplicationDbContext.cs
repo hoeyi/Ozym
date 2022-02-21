0f556201128cb932890fd50594a60c1497d543ff
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-02-21 14:35:25 -0500

    feat: add authentication pages and migrations

---
+++ EulerFinancial.Blazor/Authentication/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EulerFinancial.Blazor.Authentication
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
