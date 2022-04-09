d51a63548007a52ef71938b11454c83ce2ff6a72
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-04-09 15:43:57 -0400

    feat: update NavMenu to support nagivation.

---
+++ EulerFinancial.Web/Components/Shared/NavMenuItem.cs
@@ -0,0 +1,71 @@
+﻿namespace EulerFinancial.Web.Components.Shared
+{
+    public struct NavMenuItem
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="iconKey">The </param>
+        /// <param name="caption"></param>
+        /// <param name="uriStem"></param>
+        public NavMenuItem(string iconKey, string caption, string uriStem)
+        {
+            IconKey = iconKey;
+            Caption = caption;
+            UriStem = uriStem;
+        }
+
+        /// <summary>
+        /// Gets or sets the key for the navigation menu item.
+        /// </summary>
+        public string? IconKey { get; set; }
+
+        /// <summary>
+        /// Gets or sets the caption for the navigation menu item.
+        /// </summary>
+        public string? Caption { get; set; }
+
+        /// <summary>
+        /// Gets or sets the uri stem for the navigation menu item.
+        /// </summary>
+        public string? UriStem { get; set; }
+
+        /// <summary>
+        /// Gets whether any instance properties are undefined.
+        /// </summary>
+        public bool IsIncomplete
+        {
+            get
+            {
+                return string.IsNullOrEmpty(IconKey) ||
+                        string.IsNullOrEmpty(Caption) ||
+                        string.IsNullOrEmpty(UriStem);
+            }
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object? obj)
+        {
+            if (obj is NavMenuItem navIcon)
+                return navIcon.GetHashCode() == GetHashCode();
+            else
+                return false;
+        }
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+        {
+            return System.HashCode.Combine(IconKey, Caption, UriStem);
+        }
+
+        public static bool operator ==(NavMenuItem left, NavMenuItem right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(NavMenuItem left, NavMenuItem right)
+        {
+            return !(left == right);
+        }
+    }
+}
