7b8cfe237bc04132eca3c58b0227d7a938c5b787
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-04-17 20:51:55 -0400

    test(DAL): add helper method for comparing object property values
    Verifying unit of work requires checking property values. Add a function that handles
    the comparison and returns true if all expected properties match, else false.
    Add unit tests covering the behavior of SimplePropertiesAreEqual method.

---
+++ EulerFinancial.DAL.Test/SimplePropertiesAreEqualTest.cs
@@ -0,0 +1,85 @@
+﻿using EulerFinancial.Model;
+using EulerFinancial.ModelMetadata;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerFinancial.UnitTest
+{
+    [TestClass]
+    public class SimplePropertiesAreEqualTest
+    {
+        [TestMethod]
+        public void SimplePropertiesAreEqual_MatchedValues_Returns_True()
+        {
+            Account account1 = new()
+            {
+                AccountNavigation = new()
+                {
+                    AccountObjectCode = "TEST000_SEED",
+                    ObjectType = AccountObjectType.Account.ConvertToStringCode(),
+                    ObjectDisplayName = "TEST ACCOUNT #000",
+                    ObjectDescription = "Account used for testing purposes."
+                },
+                AccountNumber = "0000-0000-00"
+            };
+
+            Account account2 = new()
+            {
+                AccountNavigation = new()
+                {
+                    AccountObjectCode = "TEST000_SEED",
+                    ObjectType = AccountObjectType.Account.ConvertToStringCode(),
+                    ObjectDisplayName = "TEST ACCOUNT #000",
+                    ObjectDescription = "Account used for testing purposes."
+                },
+                AccountNumber = "0000-0000-00"
+            };
+
+            // Check complex objects for parity in non-complex properties.
+            Assert.IsTrue(UnitTest.SimplePropertiesAreEqual(
+                account1, account2));
+
+            Assert.IsTrue(UnitTest.SimplePropertiesAreEqual(
+                account1.AccountNavigation, account2.AccountNavigation));
+        }
+
+        [TestMethod]
+        public void SimplePropertiesAreEqual_UnmatchedValues_Returns_False()
+        {
+            Account account1 = new()
+            {
+                AccountNavigation = new()
+                {
+                    AccountObjectCode = "TEST000_SEED",
+                    ObjectType = AccountObjectType.Account.ConvertToStringCode(),
+                    ObjectDisplayName = "TEST ACCOUNT #000",
+                    ObjectDescription = "Account used for testing purposes."
+                },
+                AccountNumber = "0000-0000-00"
+            };
+
+            Account account2 = new()
+            {
+                AccountNavigation = new()
+                {
+                    AccountObjectCode = "TEST000",
+                    ObjectType = AccountObjectType.Account.ConvertToStringCode(),
+                    ObjectDisplayName = "TEST ACCOUNT #000",
+                    ObjectDescription = "Account used for testing purposes."
+                },
+                AccountNumber = "0000-0000"
+            };
+
+            // Check complex objects for parity in non-complex properties.
+            Assert.IsFalse(UnitTest.SimplePropertiesAreEqual(
+                account1, account2));
+
+            Assert.IsFalse(UnitTest.SimplePropertiesAreEqual(
+                account1.AccountNavigation, account2.AccountNavigation));
+        }
+    }
+}
