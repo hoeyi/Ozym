b3d72dae65fd7d509ded6bf190cb202a883337d9
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-04-24 20:09:08 -0400

    feat: add ModelUpdateException as public

--- EulerFinancial.DAL/Exceptions/ModelUpdateException.cs
+++ EulerFinancial.DAL/Exceptions/ModelUpdateException.cs
@@ -7,12 +7,28 @@ namespace EulerFinancial.Exceptions
     /// </summary>
     /// <remarks>Access the inner exception for the underlying exception that caused this 
     /// exception to be thrown.</remarks>
-    internal class ModelUpdateException : Exception
+    public class ModelUpdateException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ModelUpdateException"/> class.
+        /// </summary>
         public ModelUpdateException() : base() { }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ModelUpdateException"/> class with a 
+        /// specified error message.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
         public ModelUpdateException(string message) : base(message) { }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ModelUpdateException"/> class with a 
+        /// specified error message and a reference to the inner exception that is the cause of 
+        /// this exception.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception, 
+        /// or a null reference if no inner exception is specified.</param>
         public ModelUpdateException(string message, Exception innerException) 
             : base(message, innerException) { }
     }
