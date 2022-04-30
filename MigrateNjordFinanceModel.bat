a7a5159f6ac37124dd4c80a17e915d17e81525eb
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-04-30 19:20:13 -0400

    build: update migration scripts to include required parameter check

--- MigrateNjordFinanceModel.bat
+++ MigrateNjordFinanceModel.bat
@@ -1,6 +1,9 @@
 REM Change working directory to the project direct where FinanceDbContext resides.
 cd NjordFinance.DAL
 
+REM Check for expected positional parameter and redirect as needed.
+if "%1"=="" goto missingparameter
+
 REM Define context migration event.
 set migration=FinanceDbContext_%1
 
@@ -19,6 +22,14 @@ REM Remove the migration.
 dotnet ef migrations remove ^
 	--context NjordFinance.Context.FinanceDbContext ^
 	--startup-project ..\NjordFinance.Web\NjordFinance.Web.csproj
+
+goto scriptexit
+
+REM Write error message to console describing missing parameter.
+:missingparameter
+echo Missing positional parameter 'event'
+
+:scriptexit
 
 REM Revert the working directory back to the parent.
 cd ..\
--- MigrateNjordIdentityModel.bat
+++ MigrateNjordIdentityModel.bat
@@ -1,6 +1,9 @@
 REM Change working directory to the project direct where IdentityDbContext resides.
 cd NjordFinance.Web
 
+REM Check for expected positional parameter and redirect as needed.
+if "%1"=="" goto missingparameter
+
 REM Define context migration event.
 set migration=IdentityDbContext_%1
 
@@ -15,6 +18,14 @@ dotnet ef database update IdentityDbContext_Initialize ^
 
 REM Remove the migration.
 dotnet ef migrations remove
+
+goto scriptexit
+
+REM Write error message to console describing missing parameter.
+:missingparameter
+echo Missing positional parameter 'event'
+
+:scriptexit
 
 REM Revert the working directory back to the parent.
 cd ..\
