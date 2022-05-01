4c89c74e081cadc89b8d5e448a96fbd7ad98ea6b
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-05-01 18:58:06 -0400

    refactor(DAL): convert extension method to Security partial class
    Move extension method for security model to member of partial
    class defined with the model metdata class.

---
+++ NjordFinance.DAL/ModelMetadata/SecurityMetadata.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+
+namespace NjordFinance.Model
+{
+    /// <summary>
+    /// Defines metadata for <see cref="Security"/>.
+    /// </summary>
+    public class SecurityMetadata
+    {
+    }
+
+    [MetadataType(typeof(SecurityMetadata))]
+    public partial class Security
+    {
+        /// <summary>
+        /// Gets the effective <see cref="SecuritySymbol.SymbolCode"/> for this security 
+        /// as of the current date and time.
+        /// </summary>
+        /// <returns>A <see cref="string"/> representation of the symbol.</returns>
+        public string GetCurrentSymbol()
+        {
+            return SecuritySymbols?.
+                Where(s => s.EffectiveDate > DateTime.Now)
+                ?.OrderBy(s => s.EffectiveDate)
+                ?.FirstOrDefault()
+                ?.SymbolCode ?? string.Empty;
+        }
+    }
+}
