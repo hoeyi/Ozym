b13fb86b68535dbcf71e826318a839a1513cd598
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-05-01 18:59:19 -0400

    build: add test script for adding a FinanceDb migration

---
+++ AddNjordFinanceModelMigration.bat
@@ -0,0 +1,16 @@
+REM Change working directory to the project direct where FinanceDbContext resides.
+cd NjordFinance.DAL
+
+REM Check for expected positional parameter and redirect as needed.
+if "%1"=="" goto missingparameter
+
+REM Define context migration event.
+set migration=FinanceDbContext_%1
+
+REM Add the migration. Reference the FinanceDbContext with fully-qualified namespace.
+dotnet ef migrations add %migration% ^
+	--context NjordFinance.Context.FinanceDbContext ^
+	--startup-project ..\NjordFinance.Web\NjordFinance.Web.csproj ^
+	--project NjordFinance.DAL.csproj
+
+cd ..\
