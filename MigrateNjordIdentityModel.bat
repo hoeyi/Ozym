a429c5f6c72b2e034a5992bf803fd64cd0f1e305
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-04-30 21:10:48 -0400

    build: fix typo in migration script

--- MigrateNjordIdentityModel.bat
+++ MigrateNjordIdentityModel.bat
@@ -9,12 +9,14 @@ set migration=IdentityDbContext_%1
 
 REM Add the migration. Reference the IdentityDbContext with fully-qualified namespace.
 dotnet ef migrations add %migration% ^
+	--startup-project NjordFinance.Web.csproj ^
 	--context NjordFinance.Web.Data.IdentityDbContext ^
-	--project NjordFinance.Web.csproj ^
+	--project NjordFinance.Web.csproj
 
 REM Update the database using the added migration.
 dotnet ef database update %migration% ^
 	--context NjordFinance.Web.Data.IdentityDbContext ^
+	--startup-project NjordFinance.Web.csproj ^
 	--project NjordFinance.Web.csproj ^
 	--configuration Debug ^
 	--no-build
