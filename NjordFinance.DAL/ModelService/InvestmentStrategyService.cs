6d019742d8b000fb86a8b99efc305d880bdb18ca
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-05-13 17:45:27 -0400

    feat(DAL): add InvestmentStrategyService
    Resolves #47

---
+++ NjordFinance.DAL/ModelService/InvestmentStrategyService.cs
@@ -0,0 +1,37 @@
+﻿using Ichosoft.DataModel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using NjordFinance.Context;
+using NjordFinance.Model;
+using NjordFinance.ModelMetadata;
+using NjordFinance.ModelService.Abstractions;
+using System.Linq;
+
+namespace NjordFinance.ModelService
+{
+    /// <summary>
+    /// The class for servicing single CRUD requests against the <see cref="InvestmentStrategy"/> 
+    /// data store.
+    /// </summary>
+    internal class InvestmentStrategyService : ModelService<InvestmentStrategy>
+    {
+        /// <summary>
+        /// Creates a new <see cref="InvestmentStrategyService"/> instance.
+        /// </summary>
+        /// <param name="contextFactory">An <see cref="IDbContextFactory{FinanceDbContext}" /> 
+        /// instance.</param>
+        /// <param name="modelMetadata">An <see cref="IModelMetadataService"/> instance.</param>
+        /// <param name="logger">An <see cref="ILogger"/> instance.</param>
+        public InvestmentStrategyService(
+                IDbContextFactory<FinanceDbContext> contextFactory,
+                IModelMetadataService modelMetadata,
+                ILogger logger)
+            : base(contextFactory, modelMetadata, logger)
+        {
+            Reader = new ModelReaderService<InvestmentStrategy>(
+                contextFactory, modelMetadata, logger);
+            Writer = new ModelWriterService<InvestmentStrategy>(
+                contextFactory, modelMetadata, logger);
+        }
+    }
+}
