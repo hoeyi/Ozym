6ac24f540f4da91ef1e2ad4a10f236ee95592b5e
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-05-16 20:35:40 -0400

    test(DAL): add base class for batch model service tests

---
+++ NjordFinance.UnitTest.DAL/ModelService/_ModelBatchServiceTest.cs
@@ -0,0 +1,124 @@
+﻿using Ichosoft.DataModel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using NjordFinance.Context;
+using NjordFinance.ModelService;
+using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NjordFinance.UnitTest.ModelService
+{
+    /// <summary>
+    /// Base class for testing units of work done by <typeparamref name="T"/> batch model services.
+    /// </summary>
+    /// <typeparam name="T">The model type.</typeparam>
+    public abstract partial class ModelBatchServiceTest<T> : IModelBatchServiceTest<T>
+        where T : class, new()
+    {
+        /// <inheritdoc/>
+        [TestMethod]
+        public abstract void AddPendingSave_IsDirty_Is_True();
+
+        /// <inheritdoc/>
+        [TestMethod]
+        public abstract Task GetDefault_Yields_Model_Instance();
+
+        /// <inheritdoc/>
+        [TestMethod]
+        public abstract void ModelExists_KeyIsPresent_Returns_True();
+
+        /// <inheritdoc/>
+        [TestMethod]
+        public abstract void ModelExists_ModelIsPresent_Returns_True();
+
+        /// <inheritdoc/>
+        [TestMethod]
+        public abstract Task ReadAsync_Returns_Single_Model();
+
+        /// <inheritdoc/>
+        [TestMethod]
+        public abstract void RemovePendingAdd_IsDirty_Is_False();
+
+        /// <inheritdoc/>
+        [TestMethod]
+        public abstract void RemovePendingSave_IsDirty_Is_True();
+
+        /// <inheritdoc/>
+        [TestMethod]
+        public abstract Task SelectAllAsync_Returns_Model_List();
+
+        /// <inheritdoc/>
+        [TestMethod]
+        public abstract Task SelectWhereAsync_Returns_Model_ExpectedCollection();
+
+        /// <inheritdoc/>
+        [TestMethod]
+        public abstract void UpdatePendingSave_IsDirty_Is_True();
+    }
+
+    /// <summary>
+    /// Base class for testing units of work done by <typeparamref name="T"/> batch model services.
+    /// </summary>
+    /// <typeparam name="T">The model type.</typeparam>
+    public abstract partial class ModelBatchServiceTest<T>
+    {
+        /// <summary>
+        /// Gets the collection of <typeparamref name="T"/> models to add to the 
+        /// database for test purposes.
+        /// </summary>
+        protected abstract T[] TestModels { get; }
+
+        /// <summary>
+        /// Executes set up action including seeding test samples to a shared context.
+        /// </summary>
+        public abstract void Initialize();
+
+        /// <summary>
+        /// Executes clean-up action including clearing test cases from a shared context.
+        /// </summary>
+        public abstract void CleanUp();
+
+        /// <summary>
+        /// Gets the <see cref="IDbContextFactory{TContext}"/> instace for this service.
+        /// </summary>
+        protected IDbContextFactory<FinanceDbContext> DbContextFactory => UnitTest.DbContextFactory;
+
+        /// <summary>
+        /// Gets the <see cref="ILogger"/> instance for this service.
+        /// </summary>
+        protected ILogger Logger => UnitTest.Logger;
+
+        /// <summary>
+        /// Creates the <see cref="IModelBatchService{T}"/> to be tested.
+        /// </summary>
+        /// <returns></returns>
+        protected IModelBatchService<T> BuildModelService<TService>()
+        {
+            return (IModelBatchService<T>)Activator.CreateInstance(
+                typeof(TService), DbContextFactory, new ModelMetadataService(), Logger);
+        }
+
+        /// <summary>
+        /// Utility method for creating new <see cref="FinanceDbContext"/> instances.
+        /// </summary>
+        /// <returns>A new <see cref="FinanceDbContext"/> instance.</returns>
+        protected FinanceDbContext CreateDbContext()
+        {
+            return UnitTest.DbContextFactory.CreateDbContext();
+        }
+
+        /// <summary>
+        /// Seeds the given <typeparamref name="T"/> models.
+        /// </summary>
+        /// <param name="models"></param>
+        protected void SeedTestData(params T[] models)
+        {
+            using var context = CreateDbContext();
+
+            context.AddRange(models);
+
+            context.SaveChanges();
+        }
+    }
+}
