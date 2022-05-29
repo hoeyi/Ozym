6313896502ac4a793ec964106667f2f8a6f4042b
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-05-29 12:37:18 -0400

    feat: add BankTransactionCodeAttributeService
    Resolves #33

---
+++ NjordFinance.Test.DAL/ModelService/BankTransactionCodeAttributeServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NjordFinance.Model;
+using NjordFinance.ModelService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NjordFinance.Test.ModelService
+{
+    [TestClass]
+    public class BankTransactionCodeAttributeServiceTest
+        : ModelBatchServiceTest<BankTransactionCodeAttributeMemberEntry>
+    {
+        protected override Expression<Func<BankTransactionCodeAttributeMemberEntry, bool>> ParentExpression => throw new NotImplementedException();
+
+        public override void UpdatePendingSave_IsDirty_Is_True()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IModelBatchService<BankTransactionCodeAttributeMemberEntry> GetModelService()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
