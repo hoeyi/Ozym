752491926d1b83fc2a484277630e2abf62b3c0db
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-05-29 12:38:06 -0400

    feat(DAL): add BankTransactionService
    Resolves #34

---
+++ NjordFinance.DAL/ModelService/BankTransactionService.cs
@@ -0,0 +1,52 @@
+﻿using Ichosoft.DataModel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using NjordFinance.Context;
+using NjordFinance.Model;
+using NjordFinance.ModelService.Abstractions;
+
+namespace NjordFinance.ModelService
+{
+    /// <summary>
+    /// The class for servicing single CRUD requests against the <see cref="BankTransaction"/> 
+    /// data store.
+    /// </summary>
+    internal class BankTransactionService : ModelBatchService<BankTransaction>
+    {
+        /// <summary>
+        /// Creates a new <see cref="BankTransactionService"/> instance.
+        /// </summary>
+        /// <param name="contextFactory">An <see cref="IDbContextFactory{FinanceDbContext}" /> 
+        /// instance.</param>
+        /// <param name="modelMetadata">An <see cref="IModelMetadataService"/> instance.</param>
+        /// <param name="logger">An <see cref="ILogger"/> instance.</param>
+        public BankTransactionService(
+                IDbContextFactory<FinanceDbContext> contextFactory,
+                IModelMetadataService modelMetadata,
+                ILogger logger)
+            : base(contextFactory, modelMetadata, logger)
+        {
+        }
+
+        public override bool ForParent(int parentId)
+        {
+            Reader = new ModelReaderService<BankTransaction>(
+                this, _modelMetadata, _logger)
+            {
+                ParentExpression = x => x.AccountId == parentId
+            };
+
+            Writer = new ModelWriterBatchService<BankTransaction>(
+                this, _modelMetadata, _logger)
+            {
+                ParentExpression = x => x.AccountId == parentId,
+                GetDefaultModelDelegate = () => new BankTransaction()
+                {
+                    AccountId = parentId
+                }
+            };
+
+            return true;
+        }
+    }
+}
---
+++ NjordFinance.Test.DAL/ModelService/BankTransactionServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NjordFinance.Model;
+using NjordFinance.ModelService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NjordFinance.Test.ModelService
+{
+    [TestClass]
+    public class BankTransactionServiceTest : ModelBatchServiceTest<BankTransaction>
+    {
+        protected override Expression<Func<BankTransaction, bool>> ParentExpression => throw new NotImplementedException();
+
+        public override void UpdatePendingSave_IsDirty_Is_True()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IModelBatchService<BankTransaction> GetModelService()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
