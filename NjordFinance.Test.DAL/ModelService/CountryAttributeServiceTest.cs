1883a24c5fb38d1423c3649629dd4a3a78b84419
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-05-29 12:39:42 -0400

    feat(DAL): add CountryAttributeService
    Resolves #30

---
+++ NjordFinance.Test.DAL/ModelService/CountryAttributeServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NjordFinance.Model;
+using NjordFinance.ModelService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NjordFinance.Test.ModelService
+{
+    [TestClass]
+    public class CountryAttributeServiceTest : ModelBatchServiceTest<CountryAttributeMemberEntry>
+    {
+        protected override Expression<Func<CountryAttributeMemberEntry, bool>> ParentExpression => throw new NotImplementedException();
+
+        public override void UpdatePendingSave_IsDirty_Is_True()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IModelBatchService<CountryAttributeMemberEntry> GetModelService()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
