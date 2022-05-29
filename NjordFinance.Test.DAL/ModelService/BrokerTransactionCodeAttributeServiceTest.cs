9ceaeb1b4c879a6c6469512384dc73933fc033a6
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-05-29 12:38:40 -0400

    feat(DAL): add BrokerTransactionCodeAttributeService
    Resolves #31

---
+++ NjordFinance.Test.DAL/ModelService/BrokerTransactionCodeAttributeServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NjordFinance.Model;
+using NjordFinance.ModelService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NjordFinance.Test.ModelService
+{
+    [TestClass]
+    public class BrokerTransactionCodeAttributeServiceTest
+        : ModelBatchServiceTest<BrokerTransactionCodeAttributeMemberEntry>
+    {
+        protected override Expression<Func<BrokerTransactionCodeAttributeMemberEntry, bool>> ParentExpression => throw new NotImplementedException();
+
+        public override void UpdatePendingSave_IsDirty_Is_True()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IModelBatchService<BrokerTransactionCodeAttributeMemberEntry> GetModelService()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
