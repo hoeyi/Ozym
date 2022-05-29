d3f62d9fc34a542b7bcb9e18e52a1b0dbc70d6c7
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-05-29 11:29:30 -0400

    feat(DAL): add AccountAttributMemberService
    Resolves #36

---
+++ NjordFinance.DAL/ModelService/AccountAttributeMemberService.cs
@@ -0,0 +1,58 @@
+﻿using Ichosoft.DataModel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using NjordFinance.Context;
+using NjordFinance.Model;
+using NjordFinance.ModelMetadata;
+using NjordFinance.ModelService.Abstractions;
+using System.Linq;
+
+namespace NjordFinance.ModelService
+{
+    /// <summary>
+    /// The class for servicing single CRUD requests against the <see cref="MODEL"/> 
+    /// data store.
+    /// </summary>
+    internal class AccountAttributeMemberService : ModelBatchService<AccountAttributeMemberEntry>
+    {
+        /// <summary>
+        /// Creates a new <see cref="AccountAttributeMemberService"/> instance.
+        /// </summary>
+        /// <param name="contextFactory">An <see cref="IDbContextFactory{FinanceDbContext}" /> 
+        /// instance.</param>
+        /// <param name="modelMetadata">An <see cref="IModelMetadataService"/> instance.</param>
+        /// <param name="logger">An <see cref="ILogger"/> instance.</param>
+        public AccountAttributeMemberService(
+                IDbContextFactory<FinanceDbContext> contextFactory,
+                IModelMetadataService modelMetadata,
+                ILogger logger)
+            : base(contextFactory, modelMetadata, logger)
+        {
+            Reader = new ModelReaderService<AccountAttributeMemberEntry>(
+                contextFactory, modelMetadata, logger);
+            Writer = new ModelWriterBatchService<AccountAttributeMemberEntry>(
+                contextFactory, modelMetadata, logger);
+        }
+
+        public override bool ForParent(int parentId)
+        {
+            Reader = new ModelReaderService<AccountAttributeMemberEntry>(
+                this, _modelMetadata, _logger)
+            {
+                ParentExpression = x => x.AccountObjectId == parentId
+            };
+
+            Writer = new ModelWriterBatchService<AccountAttributeMemberEntry>(
+                this, _modelMetadata, _logger)
+            {
+                ParentExpression = x => x.AccountObjectId == parentId,
+                GetDefaultModelDelegate = () => new AccountAttributeMemberEntry()
+                {
+                    AccountObjectId = parentId
+                }
+            };
+
+            return true;
+        }
+    }
+}
