093aa2ee063bc5bf135b7c710673d7defc8f5368
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-06-11 17:54:50 -0400

    test(DAL): implement CountryAttributeService tests
    Related #30

--- NjordFinance.Test.DAL/ModelService/CountryAttributeServiceTest.cs
+++ NjordFinance.Test.DAL/ModelService/CountryAttributeServiceTest.cs
@@ -13,16 +13,45 @@ namespace NjordFinance.Test.ModelService
     [TestClass]
     public class CountryAttributeServiceTest : ModelBatchServiceTest<CountryAttributeMemberEntry>
     {
-        protected override Expression<Func<CountryAttributeMemberEntry, bool>> ParentExpression => throw new NotImplementedException();
+        private const int _countryId = -682;
+        protected override Expression<Func<CountryAttributeMemberEntry, bool>> ParentExpression =>
+            x => x.CountryId == _countryId;
 
+        [TestMethod]
+        public override Task ReadAsync_Returns_Single_Model()
+        {
+            return Task.CompletedTask;
+        }
+
+        [TestMethod]
+        public override async Task SelectWhereAsync_Returns_Model_ExpectedCollection()
+        {
+            var model = GetLast(ParentExpression);
+
+            var service = GetModelService();
+
+            Expression<Func<CountryAttributeMemberEntry, bool>> expression = x =>
+                x.CountryId == model.CountryId && x.AttributeMemberId == model.AttributeMemberId
+                && x.EffectiveDate == model.EffectiveDate;
+
+            var models = await service.SelectWhereAysnc(predicate: expression, maxCount: 1);
+
+            Assert.IsTrue(TestUtility.SimplePropertiesAreEqual(models.Last(), model));
+        }
+
+        [TestMethod]
         public override void UpdatePendingSave_IsDirty_Is_True()
         {
-            throw new NotImplementedException();
+            var service = GetModelService();
+
+            var model = service.SelectAllAsync().Result.FirstOrDefault();
+
+            model.Weight *= 0.5M;
+
+            Assert.IsTrue(service.IsDirty);
         }
 
-        protected override IModelBatchService<CountryAttributeMemberEntry> GetModelService()
-        {
-            throw new NotImplementedException();
-        }
+        protected override IModelBatchService<CountryAttributeMemberEntry> GetModelService() =>
+            BuildModelService<CountryAttributeService>().WithParent(parentId: _countryId);
     }
 }
