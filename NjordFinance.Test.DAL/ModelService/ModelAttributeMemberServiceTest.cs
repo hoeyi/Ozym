3c02fc6373941493e57886a7e08e70d629927a9f
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-06-11 18:27:35 -0400

    test(DAL): implement ModelAttributeMemberService tests
    Related #24

---
+++ NjordFinance.Test.DAL/ModelService/ModelAttributeMemberServiceTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NjordFinance.Model;
+using NjordFinance.ModelService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NjordFinance.Test.ModelService
+{
+    [TestClass]
+    public class ModelAttributeMemberServiceTest
+        : ModelBatchServiceTest<ModelAttributeMember>
+    {
+        private const int _attributeId = -3;
+
+        protected override Expression<Func<ModelAttributeMember, bool>> ParentExpression =>
+            x => x.AttributeId == _attributeId;
+
+        [TestMethod]
+        public override void UpdatePendingSave_IsDirty_Is_True()
+        {
+            var service = GetModelService();
+
+            var model = service.SelectAllAsync().Result.FirstOrDefault();
+
+            model.DisplayOrder++;
+
+            Assert.IsTrue(service.IsDirty);
+        }
+
+        protected override IModelBatchService<ModelAttributeMember> GetModelService() =>
+            BuildModelService<ModelAttributeMemberService>().WithParent(parentId: _attributeId);
+    }
+}
