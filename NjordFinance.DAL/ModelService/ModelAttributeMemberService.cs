f69e0d93e69e05c551e2c4f566f1d5b9aca78aa6
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-06-11 18:23:05 -0400

    feat(DAL): implement ModelAttributeMemberService
    Resolves #24

---
+++ NjordFinance.DAL/ModelService/ModelAttributeMemberService.cs
@@ -0,0 +1,53 @@
+﻿using Ichosoft.DataModel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using NjordFinance.Context;
+using NjordFinance.Model;
+using NjordFinance.ModelService.Abstractions;
+using System;
+
+namespace NjordFinance.ModelService
+{
+    /// <summary>
+    /// The class for servicing single CRUD requests against the <see cref="ModelAttributeMember"/> 
+    /// data store.
+    /// </summary>
+    internal class ModelAttributeMemberService : ModelBatchService<ModelAttributeMember>
+    {
+        /// <summary>
+        /// Creates a new <see cref="ModelAttributeMemberService"/> instance.
+        /// </summary>
+        /// <param name="contextFactory">An <see cref="IDbContextFactory{FinanceDbContext}" /> 
+        /// instance.</param>
+        /// <param name="modelMetadata">An <see cref="IModelMetadataService"/> instance.</param>
+        /// <param name="logger">An <see cref="ILogger"/> instance.</param>
+        public ModelAttributeMemberService(
+                IDbContextFactory<FinanceDbContext> contextFactory,
+                IModelMetadataService modelMetadata,
+                ILogger logger)
+            : base(contextFactory, modelMetadata, logger)
+        {
+        }
+
+        public override bool ForParent(int parentId)
+        {
+            Reader = new ModelReaderService<ModelAttributeMember>(
+                this, _modelMetadata, _logger)
+            {
+                ParentExpression = x => x.AttributeId == parentId
+            };
+
+            Writer = new ModelWriterBatchService<ModelAttributeMember>(
+                this, _modelMetadata, _logger)
+            {
+                ParentExpression = x => x.AttributeId == parentId,
+                GetDefaultModelDelegate = () => new()
+                {
+                    AttributeId = parentId
+                }
+            };
+
+            return true;
+        }
+    }
+}
