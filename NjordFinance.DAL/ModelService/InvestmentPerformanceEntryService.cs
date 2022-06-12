b842ad90f9fb30e3374b40b058a727ea77f7acb0
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-06-12 12:31:25 -0400

    feat(DAL): implement InvestmentPerformanceEntryService
    Resolves #28

---
+++ NjordFinance.DAL/ModelService/InvestmentPerformanceEntryService.cs
@@ -0,0 +1,54 @@
+﻿using Ichosoft.DataModel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using NjordFinance.Context;
+using NjordFinance.Model;
+using NjordFinance.ModelService.Abstractions;
+using System;
+
+namespace NjordFinance.ModelService
+{
+    /// <summary>
+    /// The class for servicing single CRUD requests against the <see cref="InvestmentPerformanceEntry"/> 
+    /// data store.
+    /// </summary>
+    internal class InvestmentPerformanceEntryService : ModelBatchService<InvestmentPerformanceEntry>
+    {
+        /// <summary>
+        /// Creates a new <see cref="InvestmentPerformanceEntryService"/> instance.
+        /// </summary>
+        /// <param name="contextFactory">An <see cref="IDbContextFactory{FinanceDbContext}" /> 
+        /// instance.</param>
+        /// <param name="modelMetadata">An <see cref="IModelMetadataService"/> instance.</param>
+        /// <param name="logger">An <see cref="ILogger"/> instance.</param>
+        public InvestmentPerformanceEntryService(
+                IDbContextFactory<FinanceDbContext> contextFactory,
+                IModelMetadataService modelMetadata,
+                ILogger logger)
+            : base(contextFactory, modelMetadata, logger)
+        {
+        }
+
+        public override bool ForParent(int parentId, out NotSupportedException e)
+        {
+            Reader = new ModelReaderService<InvestmentPerformanceEntry>(
+                this, _modelMetadata, _logger)
+            {
+                ParentExpression = x => x.AccountObjectId == parentId
+            };
+
+            Writer = new ModelWriterBatchService<InvestmentPerformanceEntry>(
+                this, _modelMetadata, _logger)
+            {
+                ParentExpression = x => x.AccountObjectId == parentId,
+                GetDefaultModelDelegate = () => new()
+                {
+                    AccountObjectId = parentId
+                }
+            };
+
+            e = null;
+            return true;
+        }
+    }
+}
