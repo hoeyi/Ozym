bed3ed05a5543236ffe3ef8c44b9787617bfef4a
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-06-15 21:40:50 -0400

    feat(DAL): implement alternate to Reference.IReferenceDataService

---
+++ NjordFinance.DAL/ModelService/IReferenceDataService.cs
@@ -0,0 +1,71 @@
+﻿using NjordFinance.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using NjordFinance.ModelMetadata;
+
+namespace NjordFinance.ModelService
+{
+    /// <summary>
+    /// Represents a read-only service for extracting common reference lists.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IReferenceDataService<T>
+    {
+        /// <summary>
+        /// Gets the collection of <see cref="AccountCustodian"/> models as a reference list.
+        /// </summary>
+        /// <returns>A <see cref="Task{T}"/> representing allowable selections.</returns>
+        Task<T> AccountCustodianListAsync();
+
+        /// <summary>
+        /// Gets the collection of <see cref="Account"/> models as a reference list.
+        /// </summary>
+        /// <returns>A <see cref="Task{TResult}"/> representing allowable selections.</returns>
+        Task<T> AccountListAsync();
+
+        /// <summary>
+        /// Gets the collection of <see cref="Security"/> models as a reference list, 
+        /// restricted to securities that are valid end-points for brokerage transactions.
+        /// </summary>
+        /// <returns>A <see cref="Task{TResult}"/> representing allowable selections.</returns>
+        Task<T> CashOrExternalSecurityListAsync();
+
+        /// <summary>
+        /// Gets the collection of <see cref="Country"/> models as a reference list.
+        /// </summary>
+        /// <returns>A <see cref="Task{TResult}"/> representing allowable selections.</returns>
+        Task<T> CountryListAsync();
+
+        /// <summary>
+        /// Gets the collection of <see cref="Security"/> models as a reference list, 
+        /// restricted to securities that are crypto-currencies or storable in a digital wallet.
+        /// </summary>
+        /// <returns>A <see cref="Task{TResult}"/> representing allowable selections.</returns>
+        Task<T> CryptocurrencyListAsync();
+
+        /// <summary>
+        /// Gets the collection of <see cref="ModelAttribute"/> models as a reference list, 
+        /// restricted to entries with the given <see cref="ModelAttributeScopeCode"/>.
+        /// </summary>
+        /// <returns>A <see cref="Task{TResult}"/> representing allowable selections.</returns>
+        Task<T> ModelAttributeListAsync(ModelAttributeScopeCode scopeCode);
+
+        /// <summary>
+        /// Gets the collection of <see cref="ModelAttributeMember"/> models as a reference list, 
+        /// restricted to the given <see cref="ModelAttribute.AttributeId"/>.
+        /// </summary>
+        /// <returns>A <see cref="Task{TResult}"/> representing allowable selections.</returns>
+        Task<T> ModelAttributeMemberListAsync(int attributeId);
+
+        /// <summary>
+        /// Gets the collection of <see cref="Security"/> models as a reference list, restricted to 
+        /// securities that are valid subjects for brokerage transactions.
+        /// </summary>
+        /// <returns>A <see cref="Task{TResult}"/> representing allowable selections.</returns>
+        Task<T> TransactableSecurityListAsync();
+    }
+}
---
+++ NjordFinance.DAL/ModelService/ReferenceDataService.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NjordFinance.Context;
+using NjordFinance.Model;
+using NjordFinance.ModelMetadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NjordFinance.ModelService
+{
+    public class ReferenceDataService<T> : IReferenceDataService<IDictionary<int, string>>
+    {
+        private readonly FinanceDbContext _context;
+        public ReferenceDataService(FinanceDbContext context)
+        {
+            if (context is null)
+                throw new ArgumentNullException(paramName: nameof(context));
+
+            _context = context;   
+        }
+
+        public async Task<IDictionary<int, string>> AccountCustodianListAsync()
+        {
+            var result = await _context.AccountCustodians
+                .ToDictionaryAsync(a => a.AccountCustodianId, b => b.CustodianCode);
+
+            result.Add(0, Resources.UserInterfaceString.Caption_InputSelect_Placeholder);
+
+            return result;
+        }
+
+        public async Task<IDictionary<int, string>> AccountListAsync()
+        {
+            var result = await _context.Accounts
+                .Include(a => a.AccountNavigation)
+                .ToDictionaryAsync(a => a.AccountId, b => b.AccountNavigation.AccountObjectCode);
+
+            result.Add(0, Resources.UserInterfaceString.Caption_InputSelect_Placeholder);
+
+            return result;
+        }
+
+        public async Task<IDictionary<int, string>> CashOrExternalSecurityListAsync()
+        {
+            var result = await _context.Securities
+                .Include(s => s.SecuritySymbols)
+                .Include(s => s.SecurityType)
+                .ThenInclude(s => s.SecurityTypeGroup)
+                .Where(s => s.SecurityType.SecurityTypeGroup.Transactable)
+                .ToDictionaryAsync(a => a.SecurityId, b => b.SecuritySymbol);
+
+            result.Add(0, Resources.UserInterfaceString.Caption_InputSelect_Placeholder);
+
+            return result;
+        }
+
+        public async Task<IDictionary<int, string>> CountryListAsync()
+        {
+            var result = await _context.Countries
+                .ToDictionaryAsync(a => a.CountryId, a => $"{a.DisplayName}");
+
+            result.Add(0, Resources.UserInterfaceString.Caption_InputSelect_Placeholder);
+
+            return result;
+        }
+
+        public async Task<IDictionary<int, string>> CryptocurrencyListAsync()
+        {
+            var result = await _context.Securities
+                .Include(s => s.SecuritySymbols)
+                .Include(s => s.SecurityType)
+                .Where(s => s.SecurityType.HeldInWallet)
+                .ToDictionaryAsync(a => a.SecurityId, b => b.SecuritySymbol);
+
+            result.Add(0, Resources.UserInterfaceString.Caption_InputSelect_Placeholder);
+
+            return result;
+        }
+
+        public async Task<IDictionary<int, string>> ModelAttributeMemberListAsync(int attributeId)
+        {
+            var result = await _context.ModelAttributeMembers
+                .Where(a => a.AttributeId == attributeId)
+                .ToDictionaryAsync(a => a.AttributeMemberId, b => b.DisplayName);
+
+            result.Add(0, Resources.UserInterfaceString.Caption_InputSelect_Placeholder);
+
+            return result;
+        }
+
+        public async Task<IDictionary<int, string>> ModelAttributeListAsync(ModelAttributeScopeCode scopeCode)
+        {
+            var result = await _context.ModelAttributes
+                .Include(a => a.ModelAttributeScopes)
+                .Where(a => a.ModelAttributeScopes.Any(
+                    b => b.ScopeCode == scopeCode.ConvertToStringCode()))
+                .ToDictionaryAsync(a => a.AttributeId, b => b.DisplayName);
+
+            result.Add(0, Resources.UserInterfaceString.Caption_InputSelect_Placeholder);
+
+            return result;
+        }
+
+        public async Task<IDictionary<int, string>> TransactableSecurityListAsync()
+        {
+            var result = await _context.Securities
+                .Include(s => s.SecuritySymbols)
+                .Include(s => s.SecurityType)
+                .ThenInclude(s => s.SecurityTypeGroup)
+                .Where(s => s.SecurityType.SecurityTypeGroup.Transactable)
+                .ToDictionaryAsync(a => a.SecurityId, b => b.SecuritySymbol);
+
+            result.Add(0, Resources.UserInterfaceString.Caption_InputSelect_Placeholder);
+
+            return result;
+        }
+    }
+}
