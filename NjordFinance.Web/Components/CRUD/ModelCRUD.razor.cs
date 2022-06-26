2f62ee6ce29f16c4c98eb1020d82e176ef2d984a
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-06-26 10:06:26 -0400

    refactor(Blazor): revise account index page to use async call

---
+++ NjordFinance.Web/Components/CRUD/ModelCRUD.razor.cs
@@ -0,0 +1,11 @@
+﻿namespace NjordFinance.Web.Components.CRUD
+{
+    /// <summary>
+    /// Represents a component for presentingd the user a form for creating, reading, 
+    /// updated, or deleting a single model instance.
+    /// </summary>
+    /// <typeparam name="TModel"></typeparam>
+    public partial class ModelCRUD<TModel> : LocalizableComponent
+    {
+    }
+}
