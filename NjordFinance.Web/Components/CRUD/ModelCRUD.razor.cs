b26146a73dd1570b4ee332d9e2a0ec0e572bb3fc
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-07-02 19:25:14 -0400

    refactor(Blazor): revise specialized components for CRUD operations
    Revises components to be reused for any model CRUD component in order to
    recude code duplication.

--- NjordFinance.Web/Components/CRUD/ModelCRUD.razor.cs
+++ NjordFinance.Web/Components/CRUD/ModelCRUD.razor.cs
@@ -1,4 +1,8 @@
-﻿namespace NjordFinance.Web.Components.CRUD
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace NjordFinance.Web.Components.CRUD
 {
     /// <summary>
     /// Represents a component for presentingd the user a form for creating, reading, 
@@ -7,5 +11,10 @@
     /// <typeparam name="TModel"></typeparam>
     public partial class ModelCRUD<TModel> : LocalizableComponent
     {
+        protected string[] GetEditableMemberNames() =>
+            typeof(TModel).GetProperties()
+                .Where(p => p.GetGetMethod() is not null && p.GetSetMethod() is not null)
+                .Select(p => p.Name)
+                .ToArray();
     }
 }
