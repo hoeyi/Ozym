a67d7f779ff2a9997a353970bf1f70566828eda9
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-07-11 21:27:32 -0400

    feat(DAL): implement BankTransactionCode attribute entry view model
    Resolves #77

--- NjordFinance.DAL/Model/ViewModel/BankTransactionCodeAttributeViewModel.cs
+++ NjordFinance.DAL/Model/ViewModel/BankTransactionCodeAttributeViewModel.cs
@@ -6,8 +6,22 @@ using System.Threading.Tasks;
 
 namespace NjordFinance.Model.ViewModel
 {
-    public class BankTransactionCodeAttributeViewModel
+    public class BankTransactionCodeAttributeViewModel :
+        AttributeEntryViewModel<BankTransactionCodeAttributeMemberEntry, BankTransactionCode>
     {
+        public BankTransactionCodeAttributeViewModel(
+            BankTransactionCode transactionCode, DateTime effectiveDate)
+            : base(transactionCode, effectiveDate)
+        {
+        }
 
+        public override BankTransactionCodeAttributeMemberEntry ToEntryEntity() =>
+            new()
+            {
+                AttributeMemberId = AttributeMemberId,
+                TransactionCodeId = ParentObject.TransactionCodeId,
+                EffectiveDate = EffectiveDate,
+                Weight = 100M
+            };
     }
 }
