096b1d9ffcf611b161928ad4cf87d182c432d256
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-07-11 21:29:33 -0400

    feat(DAL): add CountryAttributeViewModel and related tests
    Resolves #79

--- NjordFinance.DAL/Model/ViewModel/CountryAttributeViewModel.cs
+++ NjordFinance.DAL/Model/ViewModel/CountryAttributeViewModel.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace NjordFinance.Model.ViewModel
 {
-    public class CountryAttributeViewModel
+    public class CountryAttributeViewModel :
+        AttributeEntryViewModel<CountryAttributeMemberEntry, Country>
     {
+        public CountryAttributeViewModel(Country country, DateTime effectiveDate)
+            : base(country, effectiveDate)
+        {
+        }
+
+        public override CountryAttributeMemberEntry ToEntryEntity() =>
+            new()
+            {
+                AttributeMemberId = AttributeMemberId,
+                CountryId = ParentObject.CountryId,
+                EffectiveDate = EffectiveDate,
+                Weight = 100M
+            };
     }
 }
---
+++ NjordFinance.Test.DAL/ViewModel/CountryAttributeViewModelTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NjordFinance.Model;
+using NjordFinance.Model.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NjordFinance.Test.ViewModel
+{
+    [TestClass]
+    public class CountryAttributeViewModelTest
+    {
+        [TestMethod]
+        public void ViewModel_Constructor_CreatesInstance()
+        {
+            var viewModel = new CountryAttributeViewModel(
+                new() { CountryId = 1 }, DateTime.Now);
+
+            Assert.IsInstanceOfType(viewModel, typeof(CountryAttributeViewModel));
+        }
+
+        [TestMethod]
+        public void ViewModel_ToEntity_YieldsEntityInstance()
+        {
+            var viewModel = new CountryAttributeViewModel(
+                new() { CountryId = 1 }, DateTime.Now);
+
+            var entity = viewModel.ToEntryEntity();
+
+            Assert.IsInstanceOfType(entity, typeof(CountryAttributeMemberEntry));
+
+            Assert.IsTrue(
+                viewModel.AttributeMemberId == entity.AttributeMemberId
+                && viewModel.ParentObject.CountryId == entity.CountryId
+                && viewModel.EffectiveDate == entity.EffectiveDate
+                && entity.Weight == 100M);
+        }
+    }
+}
