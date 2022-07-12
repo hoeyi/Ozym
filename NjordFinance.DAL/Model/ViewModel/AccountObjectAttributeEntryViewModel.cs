370e055d75b0a01a80646aa5fff80706f64faa33
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-07-11 21:52:56 -0400

    feat(DAL): implement AccountObjectAttributeEntryViewModel and tests
    Resolves #76

---
+++ NjordFinance.DAL/Model/ViewModel/AccountObjectAttributeEntryViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NjordFinance.Model.ViewModel
+{
+    public class AccountObjectAttributeViewModel :
+        AttributeEntryViewModel<AccountAttributeMemberEntry, AccountObject>
+    {
+        public AccountObjectAttributeViewModel(AccountObject accountObject, DateTime effectiveDate)
+            : base(accountObject, effectiveDate)
+        {
+        }
+
+        public override AccountAttributeMemberEntry ToEntryEntity() =>
+            new()
+            {
+                AttributeMemberId = AttributeMemberId,
+                AccountObjectId = ParentObject.AccountObjectId,
+                EffectiveDate = EffectiveDate,
+                Weight = 100M
+            };
+    }
+}
