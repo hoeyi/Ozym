131af85890cfb86914efcc97f061e340d98d9b75
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-07-11 21:29:06 -0400

    feat(DAL): implement BrokerTransactionCodeAttributeViewModel and related tests
    Resolves #78

--- NjordFinance.DAL/Model/ViewModel/BrokerTransactionCodeAttributeViewModel.cs
+++ NjordFinance.DAL/Model/ViewModel/BrokerTransactionCodeAttributeViewModel.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace NjordFinance.Model.ViewModel
 {
-    public class BrokerTransactionCodeAttributeViewModel
+    public class BrokerTransactionCodeAttributeViewModel :
+        AttributeEntryViewModel<BrokerTransactionCodeAttributeMemberEntry, BrokerTransactionCode>
     {
+        public BrokerTransactionCodeAttributeViewModel(
+            BrokerTransactionCode transactionCode, DateTime effectiveDate)
+            : base(transactionCode, effectiveDate)
+        {
+        }
+
+        public override BrokerTransactionCodeAttributeMemberEntry ToEntryEntity() =>
+            new()
+            {
+                AttributeMemberId = AttributeMemberId,
+                TransactionCodeId = ParentObject.TransactionCodeId,
+                EffectiveDate = EffectiveDate,
+                Weight = 100M
+            };
     }
 }
