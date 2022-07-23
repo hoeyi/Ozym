cc8823e30c1f65e4ba0b563cb92ba8e809619335
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-07-23 17:57:11 -0400

    test(DAL): add test for InvestmentModelTarget

---
+++ NjordFinance.Test.DAL/ViewModel/InvestmentModelViewModelTest.cs
@@ -0,0 +1,365 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NjordFinance.Model;
+using NjordFinance.Model.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NjordFinance.Test.ViewModel
+{
+    [TestClass]
+    public class InvestmentModelViewModelTest
+    {
+        [TestMethod]
+        public void ViewModel_WithRelatedData_ReturnsGroupsForEachAttribute()
+        {
+            // Arrange
+            var effDate1 = DateTime.Now.AddDays(-5).Date;
+            var effDate2 = DateTime.Now.AddDays(-15).Date;
+
+            InvestmentStrategy model = new()
+            {
+                InvestmentStrategyId = 1,
+                DisplayName = "Test strategy",
+                InvestmentStrategyTargets = new List<InvestmentStrategyTarget>
+                {
+                    new()
+                    {
+                        InvestmentStrategyId = 1,
+                        AttributeMemberId = 1,
+                        EffectiveDate = effDate1,
+                        Weight = 0.75M,
+                        AttributeMember = new()
+                        {
+                            AttributeMemberId = 1,
+                            AttributeId = 1,
+                            Attribute = new(){ AttributeId = 1, DisplayName = "Group 1" },
+                            DisplayName = $"Entry 1-1"
+                        },
+                    },
+                    new()
+                    {
+                        InvestmentStrategyId = 1,
+                        AttributeMemberId = 2,
+                        EffectiveDate = effDate1,
+                        Weight = 0.25M,
+                        AttributeMember = new()
+                        {
+                            AttributeMemberId = 1,
+                            AttributeId = 1,
+                            Attribute = new(){ AttributeId = 1, DisplayName = "Group 1" },
+                            DisplayName = $"Entry 1-2"
+                        },
+                    },
+                    new()
+                    {
+                        InvestmentStrategyId = 1,
+                        AttributeMemberId = 1,
+                        EffectiveDate = effDate2,
+                        Weight = 1M,
+                        AttributeMember = new()
+                        {
+                            AttributeMemberId = 1,
+                            AttributeId = 1,
+                            Attribute = new(){ AttributeId = 1, DisplayName = "Group 1" },
+                            DisplayName = $"Entry 1-1"
+                        },
+                    },
+                    new()
+                    {
+                        InvestmentStrategyId = 1,
+                        AttributeMemberId = 3,
+                        EffectiveDate = effDate1,
+                        Weight = 1M,
+                        AttributeMember = new()
+                        {
+                            AttributeMemberId = 3,
+                            AttributeId = 2,
+                            Attribute = new(){ AttributeId = 2, DisplayName = "Group 2" },
+                            DisplayName = $"Entry 2-3"
+                        },
+                    }
+                }
+            };
+
+            // Act
+            InvestmentModelViewModel viewModel = new(model);
+
+            // Assert
+            var obsGroupCount = viewModel.AttributeTargetCollection.Count();
+            Assert.AreEqual(2, obsGroupCount);
+
+            var firstGroup = viewModel.AttributeTargetCollection.First();
+            var secondGroup = viewModel.AttributeTargetCollection.Skip(1).First();
+
+            Assert.AreEqual(2, firstGroup.Count());
+            Assert.AreEqual(1, secondGroup.Count());
+        }
+
+        [TestMethod]
+        public void ViewModel_WithRelatedData_ToEntity_YieldsValidEntity()
+        {
+            // Arrange
+            var effDate1 = DateTime.Now.AddDays(-5).Date;
+            var effDate2 = DateTime.Now.AddDays(-15).Date;
+
+            InvestmentStrategy model = new()
+            {
+                InvestmentStrategyId = 1,
+                DisplayName = "Test strategy",
+                InvestmentStrategyTargets = new List<InvestmentStrategyTarget>
+                {
+                    new()
+                    {
+                        InvestmentStrategyId = 1,
+                        AttributeMemberId = 1,
+                        EffectiveDate = effDate1,
+                        Weight = 0.75M,
+                        AttributeMember = new()
+                        {
+                            AttributeMemberId = 1,
+                            AttributeId = 1,
+                            Attribute = new(){ AttributeId = 1, DisplayName = "Group 1" },
+                            DisplayName = $"Entry 1-1"
+                        },
+                    },
+                    new()
+                    {
+                        InvestmentStrategyId = 1,
+                        AttributeMemberId = 2,
+                        EffectiveDate = effDate1,
+                        Weight = 0.25M,
+                        AttributeMember = new()
+                        {
+                            AttributeMemberId = 1,
+                            AttributeId = 1,
+                            Attribute = new(){ AttributeId = 1, DisplayName = "Group 1" },
+                            DisplayName = $"Entry 1-2"
+                        },
+                    },
+                    new()
+                    {
+                        InvestmentStrategyId = 1,
+                        AttributeMemberId = 1,
+                        EffectiveDate = effDate2,
+                        Weight = 1M,
+                        AttributeMember = new()
+                        {
+                            AttributeMemberId = 1,
+                            AttributeId = 1,
+                            Attribute = new(){ AttributeId = 1, DisplayName = "Group 1" },
+                            DisplayName = $"Entry 1-1"
+                        },
+                    },
+                    new()
+                    {
+                        InvestmentStrategyId = 1,
+                        AttributeMemberId = 3,
+                        EffectiveDate = effDate1,
+                        Weight = 1M,
+                        AttributeMember = new()
+                        {
+                            AttributeMemberId = 3,
+                            AttributeId = 2,
+                            Attribute = new(){ AttributeId = 2, DisplayName = "Group 2" },
+                            DisplayName = $"Entry 2-3"
+                        },
+                    }
+                }
+            };
+
+            InvestmentModelViewModel viewModel = new(model);
+
+            // Act
+            InvestmentStrategy convertedEntity = viewModel.ToEntity();
+
+            // Assert
+            Assert.IsTrue(TestUtility.SimplePropertiesAreEqual(model, convertedEntity));
+
+            var targetComparisons = model.InvestmentStrategyTargets.Select(entry =>
+            {
+                var comparedEntry = convertedEntity.InvestmentStrategyTargets
+                    .First(e =>
+                        e.InvestmentStrategyId == entry.InvestmentStrategyId &&
+                        e.AttributeMemberId == entry.AttributeMemberId &&
+                        e.EffectiveDate == entry.EffectiveDate);
+
+                return TestUtility.SimplePropertiesAreEqual(entry, comparedEntry);
+            });
+
+            Assert.IsTrue(model.InvestmentStrategyTargets
+                .All(entry =>
+                    {
+                        var comparedEntry = convertedEntity.InvestmentStrategyTargets
+                            .First(e =>
+                                e.InvestmentStrategyId == entry.InvestmentStrategyId &&
+                                e.AttributeMemberId == entry.AttributeMemberId &&
+                                e.EffectiveDate == entry.EffectiveDate);
+
+                        return TestUtility.SimplePropertiesAreEqual(entry, comparedEntry);
+                    }));
+        }
+
+        [TestMethod]
+        public void ViewModel_ValidDefinition_YieldsValidState()
+        {
+            // Arrange
+            var effDate1 = DateTime.Now.AddDays(-5).Date;
+            var effDate2 = DateTime.Now.AddDays(-15).Date;
+
+            InvestmentStrategy model = new()
+            {
+                InvestmentStrategyId = 1,
+                DisplayName = "Test strategy",
+                InvestmentStrategyTargets = new List<InvestmentStrategyTarget>
+                {
+                    new()
+                    {
+                        InvestmentStrategyId = 1,
+                        AttributeMemberId = 1,
+                        EffectiveDate = effDate1,
+                        Weight = 0.75M,
+                        AttributeMember = new()
+                        {
+                            AttributeMemberId = 1,
+                            AttributeId = 1,
+                            Attribute = new(){ AttributeId = 1, DisplayName = "Group 1" },
+                            DisplayName = $"Entry 1-1"
+                        },
+                    },
+                    new()
+                    {
+                        InvestmentStrategyId = 1,
+                        AttributeMemberId = 2,
+                        EffectiveDate = effDate1,
+                        Weight = 0.25M,
+                        AttributeMember = new()
+                        {
+                            AttributeMemberId = 1,
+                            AttributeId = 1,
+                            Attribute = new(){ AttributeId = 1, DisplayName = "Group 1" },
+                            DisplayName = $"Entry 1-2"
+                        },
+                    },
+                    new()
+                    {
+                        InvestmentStrategyId = 1,
+                        AttributeMemberId = 1,
+                        EffectiveDate = effDate2,
+                        Weight = 1M,
+                        AttributeMember = new()
+                        {
+                            AttributeMemberId = 1,
+                            AttributeId = 1,
+                            Attribute = new(){ AttributeId = 1, DisplayName = "Group 1" },
+                            DisplayName = $"Entry 1-1"
+                        },
+                    },
+                    new()
+                    {
+                        InvestmentStrategyId = 1,
+                        AttributeMemberId = 3,
+                        EffectiveDate = effDate1,
+                        Weight = 1M,
+                        AttributeMember = new()
+                        {
+                            AttributeMemberId = 3,
+                            AttributeId = 2,
+                            Attribute = new(){ AttributeId = 2, DisplayName = "Group 2" },
+                            DisplayName = $"Entry 2-3"
+                        },
+                    }
+                }
+            };
+
+            InvestmentModelViewModel viewModel = new(model);
+
+            var context = new ValidationContext(viewModel, serviceProvider: null, items: null);
+            var validationResults = new List<ValidationResult>();
+
+            Assert.IsTrue(Validator.TryValidateObject(viewModel, context, validationResults, true));
+        }
+
+        [TestMethod]
+        public void ViewModel_InvalidDefinition_YieldsInvalidState()
+        {
+            // Arrange
+            var effDate1 = DateTime.Now.AddDays(-5).Date;
+            var effDate2 = DateTime.Now.AddDays(-15).Date;
+
+            InvestmentStrategy model = new()
+            {
+                InvestmentStrategyId = 1,
+                InvestmentStrategyTargets = new List<InvestmentStrategyTarget>
+                {
+                    new()
+                    {
+                        InvestmentStrategyId = 1,
+                        AttributeMemberId = 1,
+                        EffectiveDate = effDate1,
+                        Weight = 0.75M,
+                        AttributeMember = new()
+                        {
+                            AttributeMemberId = 1,
+                            AttributeId = 1,
+                            Attribute = new(){ AttributeId = 1, DisplayName = "Group 1" },
+                            DisplayName = $"Entry 1-1"
+                        },
+                    },
+                    new()
+                    {
+                        InvestmentStrategyId = 1,
+                        AttributeMemberId = 2,
+                        EffectiveDate = effDate1,
+                        Weight = 0.25M,
+                        AttributeMember = new()
+                        {
+                            AttributeMemberId = 1,
+                            AttributeId = 1,
+                            Attribute = new(){ AttributeId = 1, DisplayName = "Group 1" },
+                            DisplayName = $"Entry 1-2"
+                        },
+                    },
+                    new()
+                    {
+                        InvestmentStrategyId = 1,
+                        AttributeMemberId = 1,
+                        EffectiveDate = effDate2,
+                        Weight = 1M,
+                        AttributeMember = new()
+                        {
+                            AttributeMemberId = 1,
+                            AttributeId = 1,
+                            Attribute = new(){ AttributeId = 1, DisplayName = "Group 1" },
+                            DisplayName = $"Entry 1-1"
+                        },
+                    },
+                    new()
+                    {
+                        InvestmentStrategyId = 1,
+                        AttributeMemberId = 3,
+                        EffectiveDate = effDate1,
+                        Weight = 1M,
+                        AttributeMember = new()
+                        {
+                            AttributeMemberId = 3,
+                            AttributeId = 2,
+                            Attribute = new(){ AttributeId = 2, DisplayName = "Group 2" },
+                            DisplayName = $"Entry 2-3"
+                        },
+                    }
+                }
+            };
+
+            InvestmentModelViewModel viewModel = new(model);
+
+            var context = new ValidationContext(viewModel, serviceProvider: null, items: null);
+            var validationResults = new List<ValidationResult>();
+
+            Assert.IsFalse(Validator.TryValidateObject(viewModel, context, validationResults, true));
+        }
+    }
+}
