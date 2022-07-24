bd71a5a9eef4bab5e10885ffac2aef2fc8a01334
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-07-24 16:38:12 -0400

    fix(Blazor): resolve formatting error with datagrid cell padding

---
+++ NjordFinance.Web/Components/DataGridCellType.cs
@@ -0,0 +1,23 @@
+﻿namespace NjordFinance.Web.Components
+{
+    /// <summary>
+    /// Enumerate the supported cell types.
+    /// </summary>
+    public enum DataGridCellType
+    {
+        /// <summary>
+        /// The cell contains a navigation link.
+        /// </summary>
+        Link,
+
+        /// <summary>
+        /// The cell contains raw text.
+        /// </summary>
+        Text,
+
+        /// <summary>
+        /// The cell contains a user control.
+        /// </summary>
+        Control
+    }
+}
