6e98dd95845b2b0534205a12f0fd28982ccd33d7
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-07-26 21:39:58 -0400

    feat(Blazor): add modal form event args and investment model target edit form

---
+++ NjordFinance.Web/Components/CRUD/ModalEventArgs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace NjordFinance.Web.Components.CRUD
+{
+    /// <summary>
+    /// Describes the result stemming from completion of a modal form.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ModalEventArgs<T> : EventArgs
+    {
+        /// <summary>
+        /// Describes the result of a modal dialog form.
+        /// </summary>
+        public enum ModalResult
+        {
+            CreateSubmitted,
+
+            DeleteSubmitted,
+
+            UpdateSubmitted,
+
+            RequestCancelled
+        }
+
+        /// <summary>
+        /// Gets the <typeparamref name="T"/> that is model object of the modal form.
+        /// </summary>
+        public T Model { get; init; }
+
+        /// <summary>
+        /// Gets the result of the modal form.
+        /// </summary>
+        public ModalResult Result { get; init; }
+    }
+}
