ee1bc2638c8bf8f28d7fee056003a6d3e7f8d84a
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-07-30 12:10:15 -0400

    refactor(Blazor): reorganize generic components for modal dialog forms

--- NjordFinance.Web/Components/ModalEventArgs.cs
+++ NjordFinance.Web/Components/ModalEventArgs.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace NjordFinance.Web.Components.CRUD
+namespace NjordFinance.Web.Components
 {
     /// <summary>
     /// Describes the result stemming from completion of a modal form.
@@ -8,19 +8,6 @@ namespace NjordFinance.Web.Components.CRUD
     /// <typeparam name="T"></typeparam>
     public class ModalEventArgs<T> : EventArgs
     {
-        /// <summary>
-        /// Describes the result of a modal dialog form.
-        /// </summary>
-        public enum ModalResult
-        {
-            CreateSubmitted,
-
-            DeleteSubmitted,
-
-            UpdateSubmitted,
-
-            RequestCancelled
-        }
 
         /// <summary>
         /// Gets the <typeparamref name="T"/> that is model object of the modal form.
@@ -30,6 +17,6 @@ namespace NjordFinance.Web.Components.CRUD
         /// <summary>
         /// Gets the result of the modal form.
         /// </summary>
-        public ModalResult Result { get; init; }
+        public DialogResult Result { get; init; }
     }
 }
