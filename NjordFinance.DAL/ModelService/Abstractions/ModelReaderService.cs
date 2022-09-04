a5fbb25e637fd3ab76bfac9c28e36b0e750dc3e3
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-09-04 01:47:49 -0400

    refactor(DAL): remove obsolete navigation path management from model service
    Removes obsolete references to the NavigationPathCollection class because its functionality has been superseded by the IncludeDelegate member.

--- NjordFinance.DAL/ModelService/Abstractions/ModelReaderService.cs
+++ NjordFinance.DAL/ModelService/Abstractions/ModelReaderService.cs
@@ -60,6 +60,12 @@ namespace NjordFinance.ModelService.Abstractions
         public Func<IQueryable<T>, IQueryable<T>> IncludeDelegate { get; init; } =
             (queryable) => queryable;
 
+        /// <summary>
+        /// Gets or sets the expression for filtering results to the parent id for 
+        /// this service.
+        /// </summary>
+        public Expression<Func<T, bool>> ParentExpression { get; internal init; }
+
         /// <inheritdoc/>
         public bool ModelExists(int? id)
         {
@@ -169,12 +175,6 @@ namespace NjordFinance.ModelService.Abstractions
 
             IQueryable<T> queryable = IncludeDelegate(context.Set<T>());
 
-            //// Loop through the navigation paths to include in the query.
-            //foreach (var path in PathCollection.Items)
-            //{
-            //    queryable = queryable.Include(path);
-            //}
-
             var result = await queryable
                             .Where(predicate)
                             .Take(maxCount)
@@ -194,77 +194,6 @@ namespace NjordFinance.ModelService.Abstractions
                 throw new ArgumentNullException(paramName: nameof(context));
 
             return context.Set<T>().Any(predicate);
-        }
-    }
-
-    /// <inheritdoc/>
-    internal partial class ModelReaderService<T>
-    {
-        /// <summary>
-        /// Gets the <see cref="NavigationPathCollection"/> instance for this service.
-        /// </summary>
-        private NavigationPathCollection PathCollection { get; } = new(pathLimit: 3);
-
-        /// <summary>
-        /// Gets or sets the expression for filtering results to the parent id for 
-        /// this service.
-        /// </summary>
-        public Expression<Func<T, bool>> ParentExpression { get; internal init; }
-
-        /// <summary>
-        /// Represents a collection of <see cref="Expression{Delegate}"/> navigation paths.
-        /// </summary>
-        private class NavigationPathCollection
-        {
-            public Expression<Func<T, object>> this[int i] => _navigationPaths[i];
-
-            /// <summary>
-            /// The hard limit for <see cref="_pathLimit"/>.
-            /// </summary>
-            private const short _hardPathLimit = 5;
-
-            /// <summary>
-            /// The collection of navigation paths
-            /// </summary>
-            private readonly Expression<Func<T, object>>[] _navigationPaths;
-
-            /// <summary>
-            /// Creates a new <see cref="NavigationPathCollection"/> instance with the given limit.
-            /// </summary>
-            /// <param name="pathLimit">The maximum number of paths that can be included.</param>
-            public NavigationPathCollection(short pathLimit)
-            {
-                short size = (pathLimit < _hardPathLimit) ? pathLimit : _hardPathLimit;
-                _navigationPaths = new Expression<Func<T, object>>[size];
-            }
-
-            /// <summary>
-            /// Gets the items in the collection.
-            /// </summary>
-            public IReadOnlyCollection<Expression<Func<T, object>>> Items
-            {
-                get { return _navigationPaths.Where(i => i is not null).ToArray(); }
-            }
-
-            /// <summary>
-            /// Adds the navigation path to the collection.
-            /// </summary>
-            /// <param name="navigationPath">The path to add.</param>
-            /// <exception cref="NotSupportedException">Occurs when the addition of the path would 
-            /// cause the query to exceed its path limit.</exception>
-            public void AddPath(Expression<Func<T, object>> navigationPath)
-            {
-                if (navigationPath is null)
-                    return;
-
-                var pathLimit = _navigationPaths.Length;
-
-                if (Items.Count >= pathLimit)
-                    throw new NotSupportedException(
-                        string.Format(ExceptionString.ModelService_QueryComplexityNotSupported, pathLimit));
-
-                _navigationPaths[Items.Count] = navigationPath;
-            }
         }
     }
 }
