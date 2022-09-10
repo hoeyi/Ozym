61dda7f93996df7c9065c4ab0a2ca3edfb651558
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-09-10 17:16:31 -0400

    feat(Blazor): add view models for BankTransactionCode attribute CRUD operations
    Resolves #77

--- NjordFinance.DAL/Model/ViewModel/BankTransactionCodeAttributeViewModel.cs
+++ NjordFinance.DAL/Model/ViewModel/BankTransactionCodeAttributeViewModel.cs
@@ -1,6 +1,56 @@
-﻿namespace NjordFinance.Model.ViewModel
+﻿using NjordFinance.Model.ViewModel.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NjordFinance.Model.ViewModel
 {
     public class BankTransactionCodeAttributeViewModel
+        : AttributeEntryViewModel<
+            BankTransactionCode,
+            BankTransactionCodeAttributeMemberEntry,
+            BankTransactionCodeAttriubeEntryViewModel>
     {
+        public BankTransactionCodeAttributeViewModel(BankTransactionCode transactionCode) 
+            : base(
+                  parentEntity: transactionCode,
+                  groupConstructor: (parent, attriubte,date) =>
+                  {
+                      return new BankTransactionCodeAttriubeEntryViewModel(
+                          parent, attriubte, date);
+                  },
+                  groupingConverterFunc: (grouping, parent) =>
+                  {
+                      var firstEntry = grouping.Where(x => x.AttributeMember is not null).First();
+
+                      return new(
+                          parentEntity: parent,
+                          modelAttribute: firstEntry.AttributeMember.Attribute,
+                          effectiveDate: firstEntry.EffectiveDate);
+                  },
+                  groupingFunc: (entries) =>
+                  {
+                      return entries.GroupBy(e => (e.AttributeMember.AttributeId, e.EffectiveDate));
+                  },
+                  entryMemberSelector: (parent) =>
+                  {
+                      return parent.BankTransactionCodeAttributeMemberEntries;
+                  })
+        {
+            // Check child entry records were included in the given model.
+            if (transactionCode.BankTransactionCodeAttributeMemberEntries is null)
+                throw new InvalidOperationException(
+                    message: GetIncompleteObjectGraphMessage(x => x.BankTransactionCodeAttributeMemberEntries));
+
+            // Check all child records have the ModelAttributeMember related record.
+            if (transactionCode.BankTransactionCodeAttributeMemberEntries.Any(a => a.AttributeMember is null))
+                throw new InvalidOperationException(
+                    message: GetIncompleteObjectGraphMessage(x => x.AttributeMember));
+
+            // Check all child record ModelAttributeMember records have the ModelAttribute record.
+            if (transactionCode.BankTransactionCodeAttributeMemberEntries.Any(a => a.AttributeMember.Attribute is null))
+                throw new InvalidOperationException(
+                    message: GetIncompleteObjectGraphMessage(x => x.AttributeMember.Attribute));
+        }
     }
 }
---
+++ NjordFinance.DAL/Model/ViewModel/BankTransactionCodeAttriubeEntryViewModel.cs
@@ -0,0 +1,65 @@
+﻿using NjordFinance.Model.ViewModel.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NjordFinance.Model;
+
+namespace NjordFinance.Model.ViewModel
+{
+    public class BankTransactionCodeAttriubeEntryViewModel
+        : AttributeEntryGrouping<BankTransactionCode, BankTransactionCodeAttributeMemberEntry>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BankTransactionCodeAttriubeEntryViewModel"/> 
+        /// class.
+        /// </summary>
+        /// <param name="parentEntity">The <see cref="BankTransactionCode"/> to which entries in the instance apply.</param>
+        /// <param name="modelAttribute">The <see cref="ModelAttribute"/> that entries in the instance describe.</param>
+        /// <param name="effectiveDate">The effective date for entries in the instance.</param>
+        public BankTransactionCodeAttriubeEntryViewModel(
+            BankTransactionCode parentEntity, ModelAttribute modelAttribute, DateTime effectiveDate) 
+            : base(parentEntity, modelAttribute, effectiveDate)
+        {
+        }
+
+        protected override Func<BankTransactionCode, ICollection<BankTransactionCodeAttributeMemberEntry>>
+            ParentEntryMemberFor => x => x.BankTransactionCodeAttributeMemberEntries;
+
+        protected override Func<BankTransactionCodeAttributeMemberEntry, bool> EntrySelector => x =>
+            (x.AttributeMember is null || x.AttributeMember.AttributeId == ParentAttribute.AttributeId)
+            && x.EffectiveDate == EffectiveDate;
+
+        protected override Func<BankTransactionCodeAttributeMemberEntry, decimal> WeightSelector =>
+            x => x.Weight;
+
+        public override BankTransactionCodeAttributeMemberEntry AddNewEntry()
+        {
+            BankTransactionCodeAttributeMemberEntry newEntry = new()
+            {
+                TransactionCodeId = ParentObject.TransactionCodeId,
+                EffectiveDate = EffectiveDate,
+                Weight = 1M,
+                AttributeMemberId = default,
+                AttributeMember = new()
+                {
+                    AttributeMemberId = default,
+                    AttributeId = ParentAttribute.AttributeId,
+                    Attribute = ParentAttribute
+                }
+            };
+
+            AddEntry(newEntry);
+
+            return newEntry;
+        }
+
+        protected override bool UpdateEntryEffectiveDate(
+            BankTransactionCodeAttributeMemberEntry entry, DateTime effectiveDate)
+        {
+            entry.EffectiveDate = effectiveDate;
+            return entry.EffectiveDate == effectiveDate;
+        }
+    }
+}
