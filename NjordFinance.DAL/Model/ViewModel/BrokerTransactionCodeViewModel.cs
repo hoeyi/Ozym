aedeb035a36bc4c8e560ddedf07db0c4098719b9
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-10-02 21:56:05 -0400

    feat(DAL): add view model and updated service for BrokerTransactionCode actions
    Updates prior implementation of BrokerTransactionCode view model to take advantage of new abstract base classes for weighted and unweighted attribute entry collections.
    
    Related #78

---
+++ NjordFinance.DAL/Model/ViewModel/BrokerTransactionCodeViewModel.cs
@@ -0,0 +1,155 @@
+﻿using NjordFinance.Model.Metadata;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NjordFinance.Model.Annotations;
+using NjordFinance.Model.ViewModel.Generic;
+
+namespace NjordFinance.Model.ViewModel
+{
+    [ModelAttributeSupport(
+            SupportedScopes = ModelAttributeScopeCode.BrokerTransactionCode)]
+    public class BrokerTransaactionCodeViewModel
+        : AttributeEntryUnweightedCollection<
+            BrokerTransactionCode,
+            BrokerTransactionCodeAttributeMemberEntry,
+            BrokerTransactionCodeAttributeGrouping>
+    {
+        public BrokerTransaactionCodeViewModel(BrokerTransactionCode transactionCode)
+            : base(
+                  parentEntity: transactionCode,
+                  groupConstructor: (parent, attriubte) =>
+                  {
+                      return new BrokerTransactionCodeAttributeGrouping(parent, attriubte);
+                  },
+                  groupConverter: (grouping, parent) =>
+                  {
+                      var firstEntry = grouping.Where(x => x.AttributeMember is not null).First();
+
+                      return new(
+                          parentEntity: parent,
+                          modelAttribute: firstEntry.AttributeMember.Attribute);
+                  },
+                  groupingFunc: (entries) =>
+                  {
+                      return entries.GroupBy(e => e.AttributeMember.AttributeId)
+                        .Select(g =>
+                        {
+                            var attribute = g.First().AttributeMember.Attribute;
+
+                            var group = new AttributeGrouping<
+                                ModelAttribute, BrokerTransactionCodeAttributeMemberEntry>(
+                                key: attribute, collection: g);
+
+                            return group;
+                        });
+                  },
+                  entryMemberSelector: (parent) =>
+                  {
+                      return parent.BrokerTransactionCodeAttributeMemberEntries;
+                  },
+                  entryDateSelector: (entry) => entry.EffectiveDate)
+        {
+            // Check child entry records were included in the given model.
+            if (transactionCode.BrokerTransactionCodeAttributeMemberEntries is null)
+                throw new InvalidOperationException(
+                    message: GetIncompleteObjectGraphMessage(x => x.BrokerTransactionCodeAttributeMemberEntries));
+
+            // Check all child records have the ModelAttributeMember related record.
+            if (transactionCode.BrokerTransactionCodeAttributeMemberEntries.Any(a => a.AttributeMember is null))
+                throw new InvalidOperationException(
+                    message: GetIncompleteObjectGraphMessage(x => x.AttributeMember));
+
+            // Check all child record ModelAttributeMember records have the ModelAttribute record.
+            if (transactionCode.BrokerTransactionCodeAttributeMemberEntries.Any(a => a.AttributeMember.Attribute is null))
+                throw new InvalidOperationException(
+                    message: GetIncompleteObjectGraphMessage(x => x.AttributeMember.Attribute));
+        }
+
+        [Display(
+            Name = nameof(ModelDisplay.BrokerTransactionCode_TransactionCode_Name),
+            Description = nameof(ModelDisplay.BrokerTransactionCode_TransactionCode_Description),
+            ResourceType = typeof(ModelDisplay))]
+        [Required(
+            ErrorMessageResourceName = nameof(ModelValidation.RequiredAttribute_ValidationError),
+            ErrorMessageResourceType = typeof(ModelValidation))]
+        [StringLength(3,
+            ErrorMessageResourceName = nameof(ModelValidation.StringLengthAttribute_ValidationError),
+            ErrorMessageResourceType = typeof(ModelValidation))]
+        public string TransactionCode
+        {
+            get { return ParentEntity.TransactionCode;  }
+            set
+            {
+                if (ParentEntity.TransactionCode != value)
+                    ParentEntity.TransactionCode = value;
+            }
+        }
+
+        [Display(
+            Name = nameof(ModelDisplay.BrokerTransactionCode_DisplayName_Name),
+            Description = nameof(ModelDisplay.BrokerTransactionCode_DisplayName_Description),
+            ResourceType = typeof(ModelDisplay))]
+        [Required(
+            ErrorMessageResourceName = nameof(ModelValidation.RequiredAttribute_ValidationError),
+            ErrorMessageResourceType = typeof(ModelValidation))]
+        [StringLength(32,
+            ErrorMessageResourceName = nameof(ModelValidation.StringLengthAttribute_ValidationError),
+            ErrorMessageResourceType = typeof(ModelValidation))]
+        public string DisplayName
+        {
+            get { return ParentEntity.DisplayName; }
+            set
+            {
+                if (ParentEntity.DisplayName != value)
+                    ParentEntity.DisplayName = value;
+            }
+        }
+
+        [Display(
+            Name = nameof(ModelDisplay.BrokerTransactionCode_CashEffect_Name),
+            Description = nameof(ModelDisplay.BrokerTransactionCode_CashEffect_Description),
+            ResourceType = typeof(ModelDisplay))]
+        public short CashEffect
+        {
+            get { return ParentEntity.CashEffect; }
+            set
+            {
+                if (ParentEntity.CashEffect != value)
+                    ParentEntity.CashEffect = value;
+            }
+        }
+
+        [Display(
+            Name = nameof(ModelDisplay.BrokerTransactionCode_ContributionWithdrawalEffect_Name),
+            Description = nameof(ModelDisplay.BrokerTransactionCode_ContributionWithdrawalEffect_Description),
+            ResourceType = typeof(ModelDisplay))]
+        public short ContributionWithdrawalEffect
+        {
+            get { return ParentEntity.ContributionWithdrawalEffect; }
+            set
+            {
+                if (ParentEntity.ContributionWithdrawalEffect != value)
+                    ParentEntity.ContributionWithdrawalEffect = value;
+            }
+        }
+
+        [Display(
+            Name = nameof(ModelDisplay.BrokerTransactionCode_QuantityEffect_Name),
+            Description = nameof(ModelDisplay.BrokerTransactionCode_QuantityEffect_Description),
+            ResourceType = typeof(ModelDisplay))]
+        public short QuantityEffect
+        {
+            get { return ParentEntity.QuantityEffect; }
+            set
+            {
+                if (ParentEntity.QuantityEffect != value)
+                    ParentEntity.QuantityEffect = value;
+            }
+        }
+    }
+}
