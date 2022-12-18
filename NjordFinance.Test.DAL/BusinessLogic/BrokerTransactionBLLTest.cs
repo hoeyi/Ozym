6dc3437c212ae0ef2704e5ac26850db0dda16264
hoeyi <48498580+hoeyi@users.noreply.github.com>
2022-12-18 17:15:46 -0500

    test(Logic): add/refactor tests for BrokerTransactionBLL class
    Add unit tests covering the behavior of querying open and closed tax lots.

--- NjordFinance.Test.DAL/BusinessLogic/BrokerTransactionBLLTest.cs
+++ NjordFinance.Test.DAL/BusinessLogic/BrokerTransactionBLLTest.cs
@@ -21,8 +21,8 @@ namespace NjordFinance.Test.BusinessLogic
                 {
                     new ()
                     {
-                        AccountId = 0,
-                        SecurityId = 0,
+                        AccountId = -1,
+                        SecurityId = -1,
                         TradeDate = DateTime.UtcNow.Date.AddDays(new Random().NextDouble() * -1D),
                         AcquisitionDate = DateTime.UtcNow.Date,
                         Quantity = 100M,
@@ -46,8 +46,8 @@ namespace NjordFinance.Test.BusinessLogic
                 {
                     new ()
                     {
-                        AccountId = 0,
-                        SecurityId = 0,
+                        AccountId = -1,
+                        SecurityId = -1,
                         TradeDate = DateTime.UtcNow.Date.AddDays(new Random().NextDouble() * -1D),
                         AcquisitionDate = DateTime.UtcNow.Date,
                         Quantity = 100M,
@@ -59,7 +59,7 @@ namespace NjordFinance.Test.BusinessLogic
         }
 
         [TestMethod]
-        public void GetAllBrokerTaxLots_HasOpeningTransactions_ReturnsTaxLotCollection()
+        public void GetAllTaxLots_HasOnlyOpeningActivity_ReturnsTaxLotCollection()
         {
             BrokerTransactionBLL brokerTransactionBLL = new(
                 new List<BrokerTransaction>()
@@ -69,14 +69,15 @@ namespace NjordFinance.Test.BusinessLogic
                     SampleTransaction_QuantityEffect_IsAboveZero_FromTradeDate
                 });
 
-            var taxLots = brokerTransactionBLL.GetBrokerTaxLots();
+            TaxLotStatus include = TaxLotStatus.Open | TaxLotStatus.Closed;
 
-            Assert.IsInstanceOfType(taxLots, typeof(IEnumerable<BrokerTaxLot>));
+            var taxLots = brokerTransactionBLL.GetTaxLots(include);
+
             Assert.IsTrue(taxLots.Count() == 2);
         }
 
         [TestMethod]
-        public void GetAllBrokerTaxLots_HasNoOpeningTransactions_ReturnsEmptyCollection()
+        public void GetAllTaxLots_HasOnlyNonAffectingActivity_ReturnsEmptyCollection()
         {
             BrokerTransactionBLL brokerTransactionBLL = new(
                 new List<BrokerTransaction>()
@@ -86,14 +87,13 @@ namespace NjordFinance.Test.BusinessLogic
                     SampleTransaction_QuantityEffect_IsBelowZero
                 });
 
-            var taxLots = brokerTransactionBLL.GetBrokerTaxLots();
+            var taxLots = brokerTransactionBLL.GetTaxLots(TaxLotStatus.Open | TaxLotStatus.Closed);
 
-            Assert.IsInstanceOfType(taxLots, typeof(IEnumerable<BrokerTaxLot>));
             Assert.IsTrue(!taxLots.Any());
         }
 
         [TestMethod]
-        public void GetAllBrokerTaxLots_HasOpeningAndNonOpeningTransactions_ReturnsExpectedCollection()
+        public void GetAllTaxLots_HasOpeningAndNonAffectingActivity_ReturnsExpectedCollection()
         {
             BrokerTransactionBLL brokerTransactionBLL = new(
                 new List<BrokerTransaction>()
@@ -107,11 +107,266 @@ namespace NjordFinance.Test.BusinessLogic
                     SampleTransaction_QuantityEffect_IsBelowZero
                 });
 
-            var taxLots = brokerTransactionBLL.GetBrokerTaxLots();
+            var taxLots = brokerTransactionBLL.GetTaxLots(TaxLotStatus.Open | TaxLotStatus.Closed);
 
-            Assert.IsInstanceOfType(taxLots, typeof(IEnumerable<BrokerTaxLot>));
             Assert.IsTrue(taxLots.Count() == 2);
         }
+
+        [TestMethod]
+        public void GetOpenTaxLots_HasOnlyOpeningActivity_ReturnsExpectedCollection()
+        {
+            BrokerTransactionBLL brokerTransactionBLL = new(
+                new List<BrokerTransaction>()
+                {
+                    SampleTransaction_QuantityEffect_IsAboveZero,
+                    SampleTransaction_QuantityEffect_IsAboveZero_FromTradeDate
+                });
+
+            var taxLots = brokerTransactionBLL.GetTaxLots(TaxLotStatus.Open);
+
+            Assert.IsTrue(taxLots.Count() == 2);
+        }
+
+        [TestMethod]
+        public void GetOpenTaxLots_SingleTaxLot_HasPartialClosure_ReturnsExpectedTaxLot()
+        {
+            // Set up the test data and instance. Use variable for tax-lot acquisition date to
+            // ensure tests run close to UTC midnight do not corrupt the test.
+            var taxLotDate = DateTime.UtcNow.Date;
+
+            // Create test instance. Both BrokerTransaction records will have the same
+            // AccountId, SecurityId, and TransactionId of the opening action must match 
+            // the TaxLotId of the closing action.
+            BrokerTransactionBLL transactionBLL = new(
+                new List<BrokerTransaction>()
+                {
+                    new()
+                    {
+                        TransactionId = 0,
+                        AccountId = -1,
+                        SecurityId = -1,
+                        Quantity = 100,
+                        Amount = 1000,
+                        AcquisitionDate = taxLotDate,
+                        TransactionCode = new()
+                        {
+                            TransactionCodeId = -1,
+                            QuantityEffect = 1
+                        }
+                    },
+                    new()
+                    {
+                        AccountId = -1,
+                        SecurityId = -1,
+                        Quantity = 10,
+                        Amount = 500,
+                        TransactionCode = new()
+                        {
+                            TransactionCodeId = -2,
+                            QuantityEffect = -1
+                        },
+                        TaxLotId = 0
+                    }
+                });
+
+            // Define the expected tax lot, which:
+            // - Lowers the unclosed quantity by 10
+            // - Lowers the uncloseed cost basis by 100
+            var expectedTaxLot = new BrokerTaxLot()
+            {
+                TaxLotId = 0,
+                AccountId = -1,
+                SecurityId = -1,
+                AcquisitionDate = taxLotDate,
+                OriginalCostBasis = 1000,
+                OriginalQuantity = 100,
+                UnclosedQuantity = 90
+            };
+
+            // Execute the test method.
+            var taxLots = transactionBLL.GetTaxLots(TaxLotStatus.Open);
+
+            // Confirm the count is correct.
+            Assert.IsTrue(taxLots.Count() == 1);
+
+            var observedTaxLot = taxLots.First();
+
+            // Confirm the return tax lot matches the expected tax lot.
+            Assert.AreEqual(expectedTaxLot, observedTaxLot);
+
+            // Confirm the calculated unclosed cost basis matches the expected value.
+            Assert.AreEqual(900, observedTaxLot.UnclosedCostBasis);
+        }
+
+        [TestMethod]
+        public void GetOpenTaxLots_SingleTaxLot_HasCompleteClosure_ReturnsEmptyCollection()
+        {
+            // Set up the test data and instance. Use variable for tax-lot acquisition date to
+            // ensure tests run close to UTC midnight do not corrupt the test.
+            var taxLotDate = DateTime.UtcNow.Date;
+
+            // Create test instance. Both BrokerTransaction records will have the same
+            // AccountId, SecurityId, and TransactionId of the opening action must match 
+            // the TaxLotId of the closing action. Quantity opened equals quantity closed.
+            BrokerTransactionBLL transactionBLL = new(
+                new List<BrokerTransaction>()
+                {
+                    new()
+                    {
+                        TransactionId = 0,
+                        AccountId = -1,
+                        SecurityId = -1,
+                        Quantity = 100,
+                        Amount = 1000,
+                        AcquisitionDate = taxLotDate,
+                        TransactionCode = new()
+                        {
+                            TransactionCodeId = -1,
+                            QuantityEffect = 1
+                        }
+                    },
+                    new()
+                    {
+                        AccountId = -1,
+                        SecurityId = -1,
+                        Quantity = 100,
+                        Amount = 500,
+                        TransactionCode = new()
+                        {
+                            TransactionCodeId = -2,
+                            QuantityEffect = -1
+                        },
+                        TaxLotId = 0
+                    }
+                });
+
+            // Execute the test method.
+            var taxLots = transactionBLL.GetTaxLots(TaxLotStatus.Open);
+
+            // Confirm the collection is empty.
+            Assert.IsTrue(!taxLots.Any());
+        }
+
+        [TestMethod]
+        public void GetClosedTaxLots_SingleTaxLot_HasCompleteClosure_ReturnsExpectedTaxLot()
+        {
+            // Set up the test data and instance. Use variable for tax-lot acquisition date to
+            // ensure tests run close to UTC midnight do not corrupt the test.
+            var taxLotDate = DateTime.UtcNow.Date;
+
+            // Create test instance. Both BrokerTransaction records will have the same
+            // AccountId, SecurityId, and TransactionId of the opening action must match 
+            // the TaxLotId of the closing action.
+            BrokerTransactionBLL transactionBLL = new(
+                new List<BrokerTransaction>()
+                {
+                    new()
+                    {
+                        TransactionId = 0,
+                        AccountId = -1,
+                        SecurityId = -1,
+                        Quantity = 100,
+                        Amount = 1000,
+                        AcquisitionDate = taxLotDate,
+                        TransactionCode = new()
+                        {
+                            TransactionCodeId = -1,
+                            QuantityEffect = 1
+                        }
+                    },
+                    new()
+                    {
+                        AccountId = -1,
+                        SecurityId = -1,
+                        Quantity = 100,
+                        Amount = 500,
+                        TransactionCode = new()
+                        {
+                            TransactionCodeId = -2,
+                            QuantityEffect = -1
+                        },
+                        TaxLotId = 0
+                    }
+                });
+
+            // Define the expected tax lot, which:
+            // - Lowers the unclosed quantity by 10
+            // - Lowers the uncloseed cost basis by 100
+            var expectedTaxLot = new BrokerTaxLot()
+            {
+                TaxLotId = 0,
+                AccountId = -1,
+                SecurityId = -1,
+                AcquisitionDate = taxLotDate,
+                OriginalCostBasis = 1000,
+                OriginalQuantity = 100,
+                UnclosedQuantity = 0
+            };
+
+            // Execute the test method.
+            var taxLots = transactionBLL.GetTaxLots(TaxLotStatus.Closed);
+
+            // Confirm the count is correct.
+            Assert.IsTrue(taxLots.Count() == 1);
+
+            var observedTaxLot = taxLots.First();
+
+            // Confirm the return tax lot matches the expected tax lot.
+            Assert.AreEqual(expectedTaxLot, observedTaxLot);
+
+            // Confirm the calculated unclosed cost basis matches the expected value.
+            Assert.AreEqual(0, observedTaxLot.UnclosedCostBasis);
+        }
+
+        [TestMethod]
+        public void GetClosedTaxLots_SingleTaxLot_HasPartialClosure_ReturnsEmptyCollection()
+        {
+            // Set up the test data and instance. Use variable for tax-lot acquisition date to
+            // ensure tests run close to UTC midnight do not corrupt the test.
+            var taxLotDate = DateTime.UtcNow.Date;
+
+            // Create test instance. Both BrokerTransaction records will have the same
+            // AccountId, SecurityId, and TransactionId of the opening action must match 
+            // the TaxLotId of the closing action. Quantity opened is greater than quantity closed.
+            BrokerTransactionBLL transactionBLL = new(
+                new List<BrokerTransaction>()
+                {
+                    new()
+                    {
+                        TransactionId = 0,
+                        AccountId = -1,
+                        SecurityId = -1,
+                        Quantity = 100,
+                        Amount = 1000,
+                        AcquisitionDate = taxLotDate,
+                        TransactionCode = new()
+                        {
+                            TransactionCodeId = -1,
+                            QuantityEffect = 1
+                        }
+                    },
+                    new()
+                    {
+                        AccountId = -1,
+                        SecurityId = -1,
+                        Quantity = 10,
+                        Amount = 500,
+                        TransactionCode = new()
+                        {
+                            TransactionCodeId = -2,
+                            QuantityEffect = -1
+                        },
+                        TaxLotId = 0
+                    }
+                });
+
+            // Execute the test method.
+            var taxLots = transactionBLL.GetTaxLots(TaxLotStatus.Closed);
+
+            // Confirm the collection is empty.
+            Assert.IsTrue(!taxLots.Any());
+        }
+
 
         /// <summary>
         /// Gets a new sample instance of the <see cref="BrokerTransaction"/> class, representing 
@@ -121,8 +376,8 @@ namespace NjordFinance.Test.BusinessLogic
         private static BrokerTransaction SampleTransaction_QuantityEffect_IsAboveZero => 
             new()
             {
-                AccountId = 0,
-                SecurityId = 0,
+                AccountId = -1,
+                SecurityId = -1,
                 TradeDate = DateTime.UtcNow.Date.AddDays(new Random().NextDouble() * -1D),
                 AcquisitionDate = DateTime.UtcNow.Date,
                 Quantity = 100M,
@@ -141,8 +396,8 @@ namespace NjordFinance.Test.BusinessLogic
         private static BrokerTransaction SampleTransaction_QuantityEffect_IsAboveZero_FromTradeDate 
             => new()
             {
-                AccountId = 0,
-                SecurityId = 0,
+                AccountId = -1,
+                SecurityId = -1,
                 TradeDate = DateTime.UtcNow.Date,
                 Quantity = 100M,
                 Amount = 1000M,
@@ -160,8 +415,8 @@ namespace NjordFinance.Test.BusinessLogic
         private static BrokerTransaction SampleTransaction_QuantityEffect_IsZero => 
             new()
             {
-                AccountId = 0,
-                SecurityId = 0,
+                AccountId = -1,
+                SecurityId = -1,
                 Amount = 1000M,
                 TransactionCode = new()
                 {
@@ -177,8 +432,9 @@ namespace NjordFinance.Test.BusinessLogic
         private static BrokerTransaction SampleTransaction_QuantityEffect_IsBelowZero => 
             new()
             {
-                AccountId = 0,
-                SecurityId = 0,
+                AccountId = -1,
+                SecurityId = -1,
+                Quantity = 10M,
                 Amount = 1000M,
                 TransactionCode = new()
                 {
