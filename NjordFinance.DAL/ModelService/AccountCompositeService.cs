c34576cae44df7861ca611cda619bd4eea3e8a08
hoeyi <48498580+hoeyi@users.noreply.github.com>
2023-04-30 14:36:54 -0400

    feat(DAL): add support for attributes to Account and AccountComposite
    Adds support for maintaining AccountAttributeMemberEntry records for
    Account and AccountComposite records.
    
    Resolves #120

--- NjordFinance.DAL/ModelService/AccountCompositeService.cs
+++ NjordFinance.DAL/ModelService/AccountCompositeService.cs
@@ -5,7 +5,8 @@ using NjordFinance.Context;
 using NjordFinance.Model;
 using NjordFinance.ModelMetadata;
 using NjordFinance.ModelService.Abstractions;
-
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace NjordFinance.ModelService
 {
@@ -33,6 +34,7 @@ namespace NjordFinance.ModelService
             {
                 IncludeDelegate = (queryable) => queryable
                     .Include(a => a.AccountCompositeNavigation)
+                    .ThenInclude(b => b.AccountAttributeMemberEntries)
                     .Include(a => a.AccountCompositeMembers)
             };
             Writer = new ModelWriterService<AccountComposite>(
@@ -75,13 +77,76 @@ namespace NjordFinance.ModelService
                 },
                 UpdateDelegate = async (context, model) =>
                 {
-                    var result = await context
-                        .MarkForUpdate(model)
-                        .SaveChangesAsync() > 0;
+                    var result = await UpdateGraphAsync(context, model);
 
                     return new DbActionResult<bool>(result, result);
                 }
             };
+
+        }
+
+        private static async Task<bool> UpdateGraphAsync(
+        FinanceDbContext context, AccountComposite model)
+        {
+            using var transaction = await context.Database.BeginTransactionAsync();
+
+            // Capture the currently saved entity.
+            var existingEntity = await context.AccountComposites
+                .Include(x => x.AccountCompositeNavigation)
+                .ThenInclude(y => y.AccountAttributeMemberEntries)
+                .FirstAsync(x => x.AccountCompositeId == model.AccountCompositeId);
+
+            // Mark children with altered index as deleted.
+            context.AccountAttributeMemberEntries.RemoveRange(
+                existingEntity.AccountCompositeNavigation.AccountAttributeMemberEntries
+                .Where(a => !model.AccountCompositeNavigation.AccountAttributeMemberEntries.Any(b =>
+                    b.AccountObjectId == a.AccountObjectId &&
+                    b.AttributeMemberId == a.AttributeMemberId &&
+                    b.EffectiveDate == a.EffectiveDate)));
+
+            // Update children where the index is unchanged.
+            foreach (var childEntry in model.AccountCompositeNavigation.AccountAttributeMemberEntries)
+            {
+                if (existingEntity.AccountCompositeNavigation.AccountAttributeMemberEntries.FirstOrDefault(t =>
+                    t.AccountObjectId == childEntry.AccountObjectId &&
+                    t.AttributeMemberId == childEntry.AttributeMemberId &&
+                    t.EffectiveDate == childEntry.EffectiveDate) is AccountAttributeMemberEntry match)
+                {
+                    context.Entry(match).CurrentValues.SetValues(childEntry);
+                }
+            }
+
+            // Add children where there is no current index match.
+            foreach (var childEntry in model.AccountCompositeNavigation.AccountAttributeMemberEntries
+                .Where(a => !existingEntity.AccountCompositeNavigation.AccountAttributeMemberEntries
+                    .Any(b =>
+                        b.AccountObjectId == a.AccountObjectId &&
+                        b.AttributeMemberId == a.AttributeMemberId &&
+                        b.EffectiveDate == a.EffectiveDate)))
+            {
+                // Define the new entity, set the key attributes to avoid collision with
+                // existing entities.
+                var newEntity = new AccountAttributeMemberEntry()
+                {
+                    AccountObjectId = childEntry.AccountObjectId,
+                    AttributeMemberId = childEntry.AttributeMemberId,
+                    EffectiveDate = childEntry.EffectiveDate
+                };
+
+                context.Add(newEntity);
+
+                // Update the new child entity values.
+                context.Entry(newEntity).CurrentValues.SetValues(childEntry);
+            }
+
+            // Udpate the curent values for the parameter model.
+            context.Entry(existingEntity).CurrentValues.SetValues(model);
+
+            bool result = await context.SaveChangesAsync() > 0;
+
+            await transaction.CommitAsync();
+
+            return result;
         }
     }
 }
