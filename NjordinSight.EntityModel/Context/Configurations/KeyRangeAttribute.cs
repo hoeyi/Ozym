1e8d571031bb0c4ad15c64c09f60d8a8c2dfc14a
hoeyi <48498580+hoeyi@users.noreply.github.com>
2023-05-13 13:49:36 -0400

    feat(EntityModel): add attribute for describing reserved keys for a class
    Add KeyRangeAttribute for IEntityTypeConfiguration implementors to mark the
    range of integer keys reserved for use by that class. May also be applied to classes
    implementing IDefaultConfiguration.
    
    Resolves #158

---
+++ NjordinSight.EntityModel/Context/Configurations/KeyRangeAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NjordinSight.EntityModel.Context.Configurations
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    internal sealed class KeyRangeAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="KeyRangeAttribute"/>.
+        /// </summary>
+        /// <param name="lowerBound">The inclusive start of the range.</param>
+        /// <param name="upperBound">The exclusive end of the range.</param>
+        /// <exception cref="InvalidOperationException">The value of <paramref name="lowerBound"/> 
+        /// was greater than or equal to the value of <paramref name="upperBound"/>.</exception>
+        public KeyRangeAttribute(int lowerBound, int upperBound)
+        {
+            if (lowerBound >= upperBound)
+                throw new InvalidOperationException(string.Format(
+                    Strings.Context_KeyRangeAttribute_InvalidOperationException,
+                    nameof(lowerBound),
+                    nameof(upperBound),
+                    lowerBound,
+                    upperBound));
+
+            Range = new(start: lowerBound, end: upperBound);
+        }
+
+        /// <summary>
+        /// Gets the <see cref="System.Range"/> representing reserved key values for the annotated 
+        /// class.
+        /// </summary>
+        public Range Range { get; }
+    }
+}
