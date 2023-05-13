da6db3bf503483d3a198ae524431f726c78c7395
hoeyi <48498580+hoeyi@users.noreply.github.com>
2023-05-13 13:58:18 -0400

    feat(EntityModel): add Type property to allow for specifying affected entity type

--- NjordinSight.EntityModel/Context/Configurations/KeyRangeAttribute.cs
+++ NjordinSight.EntityModel/Context/Configurations/KeyRangeAttribute.cs
@@ -17,10 +17,14 @@ namespace NjordinSight.EntityModel.Context.Configurations
         /// </summary>
         /// <param name="lowerBound">The inclusive start of the range.</param>
         /// <param name="upperBound">The exclusive end of the range.</param>
+        /// <param name="type">The entity type for which keys are reserved.</param>
         /// <exception cref="InvalidOperationException">The value of <paramref name="lowerBound"/> 
         /// was greater than or equal to the value of <paramref name="upperBound"/>.</exception>
-        public KeyRangeAttribute(int lowerBound, int upperBound)
+        public KeyRangeAttribute(int lowerBound, int upperBound, Type type)
         {
+            if (type is null)
+                throw new ArgumentNullException(paramName: nameof(type));
+
             if (lowerBound >= upperBound)
                 throw new InvalidOperationException(string.Format(
                     Strings.Context_KeyRangeAttribute_InvalidOperationException,
@@ -30,6 +34,7 @@ namespace NjordinSight.EntityModel.Context.Configurations
                     upperBound));
 
             Range = new(start: lowerBound, end: upperBound);
+            EntityType = type;
         }
 
         /// <summary>
@@ -37,5 +42,10 @@ namespace NjordinSight.EntityModel.Context.Configurations
         /// class.
         /// </summary>
         public Range Range { get; }
+
+        /// <summary>
+        /// Gets the database entity type for which keys are reserved.
+        /// </summary>
+        public Type EntityType { get; }
     }
 }
