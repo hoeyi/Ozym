d5eb141e05b3a84711ee4a7ebc5d748a9e55cc40
hoeyi <48498580+hoeyi@users.noreply.github.com>
2023-05-13 20:22:31 -0400

    fix(EntityModel): refresh migration to test updated configuration pattern

--- NjordinSight.EntityMigration/Migrations/20230513233509_FinanceDbContext_0.Designer.cs
+++ NjordinSight.EntityMigration/Migrations/20230513233509_FinanceDbContext_0.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NjordinSight.EntityModel.Context;
 
@@ -11,9 +12,10 @@ using NjordinSight.EntityModel.Context;
 namespace NjordinSight.EntityMigration.Migrations
 {
     [DbContext(typeof(FinanceDbContext))]
-    partial class FinanceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230513233509_FinanceDbContext_0")]
+    partial class FinanceDbContext_0
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
