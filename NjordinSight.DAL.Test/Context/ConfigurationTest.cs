dfe411dc75cf89182e517620d35aafe0d46b5b7d
hoeyi <48498580+hoeyi@users.noreply.github.com>
2023-05-13 23:34:32 -0400

    test(EntityModel): add unit tests for ConfigurationCollection

--- NjordinSight.DAL.Test/Context/ConfigurationTest.cs
+++ NjordinSight.DAL.Test/Context/ConfigurationTest.cs
@@ -4,18 +4,79 @@ using System.Linq;
 using NjordinSight.EntityModel.Context.Configurations;
 using NjordinSight.EntityModel.Context;
 using NjordinSight.EntityModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace NjordinSight.Test.Context
 {
+    [TestCategory("Unit")]
     [TestClass]
     public class ConfigurationTest
     {
-        private readonly Guid Guid = Guid.Parse("{B5F55FE3-F415-4A9B-9A58-0304CCFC11F7}");
+        private readonly Guid SourceGuid = Guid.Parse("{B5F55FE3-F415-4A9B-9A58-0304CCFC11F7}");
+
         [TestMethod]
-        public void ConfigurationBase_UniqueIntegerPrimaryKeys_ReturnsExpectedReservedKeys()
+        public void ConfigurationCollection_WithNoDuplicateKeys_IsValid_ReturnsTrue()
+        {
+            var configuration = new ConfigurationCollection();
+
+            configuration.AddConfiguration(
+                new EntityConfiguration<Account>(SourceGuid, new Account() { AccountId = 1 }));
+
+            configuration.AddConfiguration(
+                new EntityConfiguration<Account>(SourceGuid, new Account() { AccountId = 2 }));
+
+            Assert.IsTrue(configuration.IsValid(out _));
+        }
+
+        [TestMethod]
+        public void ConfigurationColleciton_WithDuplicateKeys_IsValid_ReturnsFalse()
+        {
+            var configuration = new ConfigurationCollection();
+
+            configuration.AddConfiguration(
+                new EntityConfiguration<Account>(SourceGuid, new Account() { AccountId = 1 }));
+
+            configuration.AddConfiguration(
+                new EntityConfiguration<Account>(SourceGuid, new Account() { AccountId = 1 }));
+
+            bool isValid = configuration.IsValid(out IEnumerable<string> _);
+            Assert.IsFalse(isValid);
+        }
+
+        [TestMethod]
+        public void ConfigurationColleciton_WithDuplicateKeys_WithDifferentSources_IsValid_ReturnsFalse()
+        {
+            var configuration = new ConfigurationCollection();
+
+            configuration.AddConfiguration(
+                new EntityConfiguration<Account>(SourceGuid, new Account() { AccountId = 1 }));
+
+            configuration.AddConfiguration(
+                new EntityConfiguration<Account>(Guid.NewGuid(), new Account() { AccountId = 1 }));
+
+            bool isValid = configuration.IsValid(out IEnumerable<string> _);
+            Assert.IsFalse(isValid);
+        }
+
+        [TestMethod]
+        public void Configuration_Collection_WithDuplicatKeysForDifferentEntities_IsValid_ReturnsTrue()
+        {
+            var configuration = new ConfigurationCollection();
+
+            configuration.AddConfiguration(
+                new EntityConfiguration<Account>(SourceGuid, new Account() { AccountId = 1 }));
+
+            configuration.AddConfiguration(
+                new EntityConfiguration<Country>(SourceGuid, new Country() { CountryId = 2 }));
+
+            Assert.IsTrue(configuration.IsValid(out _));
+        }
+
+        [TestMethod]
+        public void EntityConfiguration_UniqueIntegerPrimaryKeys_ReturnsExpectedReservedKeys()
         {
             var configuration = new EntityConfiguration<Account>(
-                sourceGuid: Guid,
+                sourceGuid: SourceGuid,
                 new Account[]
                 {
                     new Account() { AccountId = 1 },
@@ -31,22 +92,17 @@ namespace NjordinSight.Test.Context
         }
 
         [TestMethod]
-        public void ConfigurationBase_NonUniqueIntegerPrimaryKeys_ReturnsExpectedReservedKeys()
+        public void EntityConfiguration_NonUniqueIntegerPrimaryKeys_ReturnsExpectedReservedKeys()
         {
             var configuration = new EntityConfiguration<Account>(
-                sourceGuid: Guid,
+                sourceGuid: SourceGuid,
                 new Account[]
                 {
                     new Account() { AccountId = 1 },
                     new Account() { AccountId = 1 }
                 });
 
-            Assert.AreNotEqual(
-                configuration.ReservedKeys.Distinct().Count(),
-                configuration.ReservedKeys.Count);
-            Assert.AreNotEqual(2, configuration.ReservedKeys.Count);
-            Assert.IsTrue(configuration.ReservedKeys.Any(x => x.GetHashCode() == 1));
-            Assert.IsFalse(configuration.ReservedKeys.Any(x => x.GetHashCode() == 2));
+            Assert.AreEqual(1, configuration.ReservedKeys.Count);
         }
 
         [TestMethod]
