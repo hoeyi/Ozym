2fd8156fe4bedcdba9cb6c6d1c7d913ab1f56f08
hoeyi <48498580+hoeyi@users.noreply.github.com>
2023-06-10 17:19:30 -0400

    feat(Logic): convert Clamp method to generic with IComparable constraint

--- NjordinSight.DAL/BusinessLogic/Functions/BusinessMath.cs
+++ NjordinSight.DAL/BusinessLogic/Functions/BusinessMath.cs
@@ -15,18 +15,31 @@ namespace NjordinSight.BusinessLogic.Functions
         /// Confines the input <paramref name="value"/> to the range defined by inputs 
         /// <paramref name="min"/> and <paramref name="max"/>.
         /// </summary>
-        /// <param name="value">The value to be clamped.</param>
+        /// <param name="value">The value to be confined.</param>
         /// <param name="min">The minimum allowable value.</param>
         /// <param name="max">Teh maximum allowable value.</param>
         /// <returns>The value of <paramref name="min"/> if <paramref name="value"/> is lesser, 
         /// the value of <paramref name="max"/> if <paramref name="value"/> is greater, else 
         /// <paramref name="value"/>.</returns>
-        public static int Clamp(int value, int min, int max)
+        public static T Clamp<T>(T value, T min, T max)
+            where T : IComparable<T>
         {
             // if value is less than min, return min
             // if value is greater than min and less greater than max return max
             // else return value
-            return (value < min) ? min : (value > max) ? max : value;
+            return (value.IsLessThan(min)) ? min : (value.IsGreaterThan(max)) ? max : value;
+        }
+
+        public static bool IsGreaterThan<T>(this T value, T other) 
+            where T : IComparable<T>
+        {
+            return value.CompareTo(other) > 0;
+        }
+
+        public static bool IsLessThan<T>(this T value, T other)
+            where T : IComparable<T>
+        {
+            return value.CompareTo(other) < 0;
         }
     }
 }
