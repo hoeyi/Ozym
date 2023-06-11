99fbd850344063a1199f473834ca74d029e87b14
hoeyi <48498580+hoeyi@users.noreply.github.com>
2023-06-10 21:02:48 -0400

    feat(Logic): add IFinancialCalculator interface and basic records
    Add IFinancialCalculator interface to represent a service class that can
    provide basic business-related calculations.
    Add basic record types useful in IFinancialCalculator functions.
    
    Related #191

---
+++ NjordinSight.DAL/BusinessLogic/Functions/FutureValueResult.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NjordinSight.BusinessLogic.Functions
+{
+    /// <summary>
+    /// Represents the future value result of a single period in a series.
+    /// </summary>
+    public record FutureValueResult
+    {
+        /// <summary>
+        /// Get or sets the period index this record represents. Generally a one-based index.
+        /// </summary>
+        public int Period { get; init; }
+
+        /// <summary>
+        /// Gets or sets the ending date of the period.
+        /// </summary>
+        public DateTime PeriodDate { get; init; }
+
+        /// <summary>
+        /// Gets or sets the cumulative principal at the start of this period.
+        /// </summary>
+        public float Principal { get; init; }
+
+        /// <summary>
+        /// Gets or sets the interest earned at the end of this period.
+        /// </summary>
+        public float Interest { get; init; }
+    }
+}
