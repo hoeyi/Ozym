288a78aedcad771a45a6767e6a31dffe7db9d810
hoeyi <48498580+hoeyi@users.noreply.github.com>
2023-06-16 14:20:56 -0400

    test(Logic): add simple tests for WatchList service

---
+++ NjordinSight.DAL.Test/BusinessLogic/WatchListTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NjordinSight.BusinessLogic.Functions;
+using NjordinSight.BusinessLogic.MarketFeed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NjordinSight.Test.BusinessLogic
+{
+    [TestClass]
+    [TestCategory("Unit")]
+    public class WatchListTest
+    {
+        [TestMethod]
+        public void GetQuotes_CalledOnce_ReturnsPriceDeltas_EqualToZero()
+        {
+            var facade = new WatchList(new StatisticsCalculator());
+            var quotes = facade.GetQuotes();
+
+            Assert.IsTrue(quotes.All(x => x.Change == 0));
+        }
+
+        [TestMethod]
+        public void GetQuotes_CalledTwice_ReturnsPriceDeltas_NotEqualToZero()
+        {
+            var facade = new WatchList(new StatisticsCalculator());
+            var quotes = facade.GetQuotes();
+            quotes = facade.GetQuotes();
+
+            Assert.IsTrue(quotes.All(x => x.Change != 0));
+        }
+    }
+}
