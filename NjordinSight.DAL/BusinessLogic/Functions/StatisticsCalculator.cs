3a380fab2f91f975f8fe9752543ed2fdcbf2ed85
hoeyi <48498580+hoeyi@users.noreply.github.com>
2023-06-16 12:28:38 -0400

    feat(Logic): add calculator services for basic distribution sampling
    Adds dependency to MathNet.Numerics.

---
+++ NjordinSight.DAL/BusinessLogic/Functions/StatisticsCalculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.Distributions;
+
+namespace NjordinSight.BusinessLogic.Functions
+{
+    /// <summary>
+    /// Represents a stateless class providing statistics-related mathematical functions.
+    /// </summary>
+    public class StatisticsCalculator : IStatisticsCalculator
+    {
+        /// <inheritdoc/>
+        public double[] NormalDistributionSamples(double mean, double stddev, int count)
+        {
+            if (count <= 0)
+                throw new ArgumentOutOfRangeException(
+                    paramName: nameof(count), actualValue: count, message: nameof(NormalDistributionSamples));
+
+            var normal = new Normal(mean, stddev);
+
+            var results = new double[count];
+
+            normal.Samples(results);
+
+            return results;
+        }
+
+
+        /// <inheritdoc/>
+        public double NormalDistributionSample(double mean, double stddev)
+        {
+            var normal = new Normal(mean, stddev);
+
+            return normal.Sample();
+        }
+    }
+}
