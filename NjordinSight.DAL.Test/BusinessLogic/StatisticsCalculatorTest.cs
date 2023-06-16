37a50075f2a51a3d6ac93d83fa551a36af18d9f0
hoeyi <48498580+hoeyi@users.noreply.github.com>
2023-06-16 12:55:04 -0400

    test(Logic): add simple tests for StatisticsCaclulator

---
+++ NjordinSight.DAL.Test/BusinessLogic/StatisticsCalculatorTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NjordinSight.BusinessLogic.Functions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NjordinSight.Test.BusinessLogic
+{
+    [TestClass]
+    [TestCategory("Unit")]
+    public class StatisticsCalculatorTest
+    {
+        [TestMethod]
+        public void NormalDistribution_StandardNormal_ReturnsDouble()
+        {
+            var calculator = new StatisticsCalculator();
+
+            var observed = calculator.NormalDistributionSample(0, 1);
+
+            Assert.IsInstanceOfType(observed, typeof(double));
+        }
+
+        [TestMethod]
+        public void NormalDistributionSamples_StandardNormal_ReturnsArray()
+        {
+            var calculator = new StatisticsCalculator();
+
+            var observed = calculator.NormalDistributionSamples(0, 1, 100);
+
+            Assert.IsInstanceOfType(observed, typeof(double[]));
+            Assert.IsTrue(observed.Length == 100);
+            CollectionAssert.AllItemsAreNotNull(observed);
+        }
+    }
+}
