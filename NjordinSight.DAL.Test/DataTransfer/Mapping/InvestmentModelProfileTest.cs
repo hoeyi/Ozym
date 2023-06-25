46656031fb5fea26ef1d54148a0fcad616a719d5
hoeyi <48498580+hoeyi@users.noreply.github.com>
2023-06-25 10:54:04 -0400

    feat(DAL): add InvestmentModel mapping profile
    Related #208

---
+++ NjordinSight.DAL.Test/DataTransfer/Mapping/InvestmentModelProfileTest.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NjordinSight.DataTransfer.Profiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NjordinSight.Test.DataTransfer.Mapping
+{
+    [TestClass]
+    [TestCategory("Unit")]
+    public class InvestmentModelProfileTest : ProfileBaseTest
+    {
+        public InvestmentModelProfileTest() 
+            : base(configuration: new(x =>
+            {
+                x.AddProfile<ModelAttributeProfile>();
+                x.AddProfile<InvestmentModelProfile>();
+            }))
+        {
+        }
+
+        [TestMethod]
+        public void Configuration_WithProfile_IsValid()
+        {
+            // Assert
+            Configuration.AssertConfigurationIsValid();
+        }
+    }
+}
