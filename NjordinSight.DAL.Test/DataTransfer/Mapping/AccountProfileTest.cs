3d66fbd527b186b4c920dfebefd23f269f2b135f
hoeyi <48498580+hoeyi@users.noreply.github.com>
2023-06-27 19:29:39 -0400

    test(DAL): modify tests to include attribute comparison
    Revise AccontProfileTest to include complex property in comparion of mapped
    properties. Extract these methods as an interface for test classes targeting Profile-
    derived classes.

--- NjordinSight.DAL.Test/DataTransfer/Mapping/AccountProfileTest.cs
+++ NjordinSight.DAL.Test/DataTransfer/Mapping/AccountProfileTest.cs
@@ -5,13 +5,14 @@ using NjordinSight.EntityModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace NjordinSight.Test.DataTransfer.Mapping
 {
     [TestClass]
     [TestCategory("Unit")]
-    public class AccountProfileTest : IProfileTest
+    public class AccountProfileTest : IProfileTest, IAccountProfileTest
     {
         [TestMethod]
         public void Configuration_WithProfileDependencies_IsValid()
@@ -41,12 +42,12 @@ namespace NjordinSight.Test.DataTransfer.Mapping
             // Act
 
             // Assert
-            Assert.ThrowsException<AutoMapperConfigurationException>(() => 
-            config.AssertConfigurationIsValid());
+            Assert.ThrowsException<AutoMapperConfigurationException>(() =>
+                config.AssertConfigurationIsValid());
         }
 
         [TestMethod]
-        public async Task Account_MapTo_AccountDto_PopulatesAllProperties()
+        public async Task Dto_MapFrom_Entity_MappedProperties_AreEqual()
         {
             // Arrange
             var account = new Account()
@@ -55,7 +56,7 @@ namespace NjordinSight.Test.DataTransfer.Mapping
                 AccountNumber = "####-####-####",
                 HasBankTransaction = true,
                 HasBrokerTransaction = true,
-                AccountCustodianId  = 1,
+                AccountCustodianId = 1,
                 AccountNavigation = new()
                 {
                     AccountObjectId = 1,
@@ -66,9 +67,9 @@ namespace NjordinSight.Test.DataTransfer.Mapping
                     StartDate = DateTime.UtcNow.Date,
                     AccountAttributeMemberEntries = new List<AccountAttributeMemberEntry>()
                     {
-                        new() 
-                        { 
-                            AccountObjectId = 1, 
+                        new()
+                        {
+                            AccountObjectId = 1,
                             AttributeMemberId = 1,
                             AttributeMember = new()
                             {
@@ -81,26 +82,8 @@ namespace NjordinSight.Test.DataTransfer.Mapping
                                     DisplayName = "Attribute 1"
                                 }
                             },
-                            EffectiveDate = DateTime.UtcNow.Date, 
-                            Weight = 0.5M 
-                        },
-                        new() 
-                        { 
-                            AccountObjectId = 1, 
-                            AttributeMemberId = 2, 
-                            AttributeMember = new()
-                            {
-                                AttributeMemberId = 2,
-                                DisplayName = "Member 2",
-                                DisplayOrder = 2,
-                                Attribute = new()
-                                {
-                                    AttributeId = 1,
-                                    DisplayName = "Attribute 1"
-                                }
-                            },
-                            EffectiveDate = DateTime.UtcNow.Date, 
-                            Weight = 0.5M 
+                            EffectiveDate = DateTime.UtcNow.Date,
+                            Weight = 1M
                         }
                     }
                 }
@@ -120,7 +103,7 @@ namespace NjordinSight.Test.DataTransfer.Mapping
 
             // Fact: Attributes property is non-empty collection with count matching source.
             Assert.AreEqual(
-                accountDto.Attributes.Count, 
+                accountDto.Attributes.Count,
                 account.AccountNavigation.AccountAttributeMemberEntries.Count);
 
             // Fact: All attributes have AttributeMember complex property defined.
@@ -143,7 +126,11 @@ namespace NjordinSight.Test.DataTransfer.Mapping
                 Task.Run(() => Assert.AreEqual(accountDto.AccountNumber, account.AccountNumber)),
                 Task.Run(() => Assert.AreEqual(accountDto.HasWallet, account.HasWallet)),
                 Task.Run(() => Assert.AreEqual(accountDto.HasBankTransaction, account.HasBankTransaction)),
-                Task.Run(() => Assert.AreEqual(accountDto.HasBrokerTransaction, account.HasBrokerTransaction))
+                Task.Run(() => Assert.AreEqual(accountDto.HasBrokerTransaction, account.HasBrokerTransaction)),
+                Task.Run(() => Assert.IsTrue(
+                    Compare(
+                        attributeDto: accountDto.Attributes.First(),
+                        attribute: account.AccountNavigation.AccountAttributeMemberEntries.First())))
             };
 
             Task assertions = Task.WhenAll(comparisons);
@@ -156,7 +143,7 @@ namespace NjordinSight.Test.DataTransfer.Mapping
         }
 
         [TestMethod]
-        public async Task Account_MapFrom_AccountDto_PopulatesAllProperties()
+        public async Task Entity_MapFrom_Dto_MappedProperties_AreEqual()
         {
             // Arrange
             var accountDto = new AccountDto()
@@ -187,131 +174,10 @@ namespace NjordinSight.Test.DataTransfer.Mapping
                             }
                         },
                         EffectiveDate = DateTime.UtcNow.Date,
-                        PercentWeight = 0.5M
+                        PercentWeight = 1M
                     },
-                    new()
-                    {
-                        AccountObjectId = 1,
-                        AttributeMember = new()
-                        {
-                            AttributeMemberId = 2,
-                            DisplayName = "Member 2",
-                            DisplayOrder = 2,
-                            Attribute = new()
-                            {
-                                AttributeId = 1,
-                                DisplayName = "Attribute 1"
-                            }
-                        },
-                        EffectiveDate = DateTime.UtcNow.Date,
-                        PercentWeight = 0.5M
-                    }
                 }
             };
-            var mapper = new Mapper(new MapperConfiguration(x =>
-            {
-                x.AddProfile<ModelAttributeProfile>();
-                x.AddProfile<AccountProfile>();
-            }));
-
-            // Act
-            var account = mapper.Map<Account>(accountDto);
-
-            // Assert
-            // Fact: Instance is created.
-            Assert.IsInstanceOfType(account, typeof(Account));
-
-            // Fact: Attributes property is non-empty collection with count matching source.
-            Assert.AreEqual(
-                accountDto.Attributes.Count,
-                account.AccountNavigation.AccountAttributeMemberEntries.Count);
-
-            // Fact: All property values match.
-            var comparisons = new List<Task>() 
-            { 
-                Task.Run(() => Assert.AreEqual(accountDto.Id, account.AccountNavigation.AccountObjectId)),
-                Task.Run(() => Assert.AreEqual(accountDto.ShortCode, account.AccountNavigation.AccountObjectCode)),
-                Task.Run(() => Assert.AreEqual(accountDto.StartDate, account.AccountNavigation.StartDate)),
-                Task.Run(() => Assert.AreEqual(accountDto.CloseDate, account.AccountNavigation.CloseDate)),
-                Task.Run(() => Assert.AreEqual(accountDto.DisplayName, account.AccountNavigation.ObjectDisplayName)),
-                Task.Run(() => Assert.AreEqual(accountDto.Description, account.AccountNavigation.ObjectDescription)),
-                Task.Run(() => Assert.AreEqual(accountDto.ObjectType, account.AccountNavigation.ObjectType)),
-                Task.Run(() => Assert.AreEqual(accountDto.AccountCustodianId, account.AccountCustodianId)),
-                Task.Run(() => Assert.AreEqual(accountDto.AccountNumber, account.AccountNumber)),
-                Task.Run(() => Assert.AreEqual(accountDto.HasWallet, account.HasWallet)),
-                Task.Run(() => Assert.AreEqual(accountDto.HasBankTransaction, account.HasBankTransaction)),
-                Task.Run(() => Assert.AreEqual(accountDto.HasBrokerTransaction, account.HasBrokerTransaction))
-            };
-
-            Task assertions = Task.WhenAll(comparisons);
-            await assertions;
-
-            if(assertions.Status == TaskStatus.Faulted)
-            {
-                Assert.Fail();
-            }
-        }
-
-        [TestMethod]
-        public async Task Account_MapFrom_AccountDto_WithAddedAttributes_PopulatesAllProperties()
-        {
-            var accountDto = new AccountDto()
-            {
-                Id = 1,
-                AccountNumber = "####-####-####",
-                HasBankTransaction = true,
-                HasBrokerTransaction = true,
-                AccountCustodianId = 1,
-                ShortCode = "TEST",
-                Description = "For testing purposes.",
-                DisplayName = "Test Account",
-                StartDate = DateTime.UtcNow.Date,
-                Attributes = new List<AccountBaseAttributeDto>()
-                {
-                    new()
-                    {
-                        AccountObjectId = 1,
-                        AttributeMember = new()
-                        {
-                            AttributeMemberId = 1,
-                            DisplayName = "Member 1",
-                            DisplayOrder = 1,
-                            Attribute = new()
-                            {
-                                AttributeId = 1,
-                                DisplayName = "Attribute 1"
-                            }
-                        },
-                        EffectiveDate = DateTime.UtcNow.Date,
-                        PercentWeight = 0.5M
-                    },
-                    new()
-                    {
-                        AccountObjectId = 1,
-                        AttributeMember = new()
-                        {
-                            AttributeMemberId = 2,
-                            DisplayName = "Member 2",
-                            DisplayOrder = 2,
-                            Attribute = new()
-                            {
-                                AttributeId = 1,
-                                DisplayName = "Attribute 1"
-                            }
-                        },
-                        EffectiveDate = DateTime.UtcNow.Date,
-                        PercentWeight = 0.5M
-                    }
-                }
-            };
-
-            accountDto.AttributeCollection.AddEntryForGrouping(
-                new ModelAttributeDto()
-                {
-                    AttributeId = 10,
-                    DisplayName = "Test added attribute"
-                });
-
             var mapper = new Mapper(new MapperConfiguration(x =>
             {
                 x.AddProfile<ModelAttributeProfile>();
@@ -344,7 +210,11 @@ namespace NjordinSight.Test.DataTransfer.Mapping
                 Task.Run(() => Assert.AreEqual(accountDto.AccountNumber, account.AccountNumber)),
                 Task.Run(() => Assert.AreEqual(accountDto.HasWallet, account.HasWallet)),
                 Task.Run(() => Assert.AreEqual(accountDto.HasBankTransaction, account.HasBankTransaction)),
-                Task.Run(() => Assert.AreEqual(accountDto.HasBrokerTransaction, account.HasBrokerTransaction))
+                Task.Run(() => Assert.AreEqual(accountDto.HasBrokerTransaction, account.HasBrokerTransaction)),
+                Task.Run(() => Assert.IsTrue(
+                    Compare(
+                        attributeDto: accountDto.Attributes.First(),
+                        attribute: account.AccountNavigation.AccountAttributeMemberEntries.First())))
             };
 
             Task assertions = Task.WhenAll(comparisons);
@@ -356,6 +226,13 @@ namespace NjordinSight.Test.DataTransfer.Mapping
             }
         }
 
-        
+        private static bool Compare(
+            AccountBaseAttributeDto attributeDto, AccountAttributeMemberEntry attribute)
+        {
+            return attribute.AttributeMemberId == attributeDto.AttributeMember.AttributeMemberId
+                && attribute.EffectiveDate == attributeDto.EffectiveDate 
+                && attribute.AccountObjectId == attributeDto.AccountObjectId
+                && attribute.Weight == attributeDto.PercentWeight;
+        }
     }
 }
