92d66ba0f3336973e560f5a45ad20601693e6e85
hoeyi <48498580+hoeyi@users.noreply.github.com>
2023-06-27 19:29:39 -0400

    test(DAL): add tests for account attribute mapping
    Add test to verify expected behavior when mapping account attributes that
    have been added, removed, or updated.
    Adds checks to related tests to confirm expected count of collection after action.

--- NjordinSight.DAL.Test/DataTransfer/Mapping/AccountProfileTest.cs
+++ NjordinSight.DAL.Test/DataTransfer/Mapping/AccountProfileTest.cs
@@ -5,6 +5,7 @@ using NjordinSight.EntityModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace NjordinSight.Test.DataTransfer.Mapping
@@ -42,7 +43,7 @@ namespace NjordinSight.Test.DataTransfer.Mapping
 
             // Assert
             Assert.ThrowsException<AutoMapperConfigurationException>(() => 
-            config.AssertConfigurationIsValid());
+                config.AssertConfigurationIsValid());
         }
 
         [TestMethod]
@@ -253,8 +254,9 @@ namespace NjordinSight.Test.DataTransfer.Mapping
         }
 
         [TestMethod]
-        public async Task Account_MapFrom_AccountDto_WithAddedAttributes_PopulatesAllProperties()
+        public void Account_MapFrom_AccountDto_WithAddedAttributes_YieldsPlusOneAttributeEntry()
         {
+            // Arrange
             var accountDto = new AccountDto()
             {
                 Id = 1,
@@ -304,13 +306,93 @@ namespace NjordinSight.Test.DataTransfer.Mapping
                     }
                 }
             };
+            var initialCountPlusOne = accountDto.Attributes.Count + 1;
+            
+            var mapper = new Mapper(new MapperConfiguration(x =>
+            {
+                x.AddProfile<ModelAttributeProfile>();
+                x.AddProfile<AccountProfile>();
+            }));
 
+            // Act
             accountDto.AttributeCollection.AddEntryForGrouping(
                 new ModelAttributeDto()
                 {
                     AttributeId = 10,
                     DisplayName = "Test added attribute"
                 });
+            var account = mapper.Map<Account>(accountDto);
+
+            // Assert
+            // Fact: Instance is created.
+            Assert.IsInstanceOfType(account, typeof(Account));
+
+            // Fact: Attributes property is non-empty collection with count matching source 
+            //       and expected count from the arrangment.
+            Assert.AreEqual(
+                accountDto.Attributes.Count,
+                account.AccountNavigation.AccountAttributeMemberEntries.Count);
+
+            Assert.AreEqual(
+                expected: initialCountPlusOne,
+                actual: account.AccountNavigation.AccountAttributeMemberEntries.Count);
+        }
+
+        [TestMethod]
+        public void Account_MapFrom_AccountDto_WithDeletedAttributes_YieldsMinusOneAttributeEntry()
+        {
+            // Arrange
+            var testAttributeDto = new AccountBaseAttributeDto()
+            {
+                AccountObjectId = 1,
+                AttributeMember = new()
+                {
+                    AttributeMemberId = 2,
+                    DisplayName = "Member 2",
+                    DisplayOrder = 2,
+                    Attribute = new()
+                    {
+                        AttributeId = 1,
+                        DisplayName = "Attribute 1"
+                    }
+                },
+                EffectiveDate = DateTime.UtcNow.Date,
+                PercentWeight = 0.5M
+            };
+            var accountDto = new AccountDto()
+            {
+                Id = 1,
+                AccountNumber = "####-####-####",
+                HasBankTransaction = true,
+                HasBrokerTransaction = true,
+                AccountCustodianId = 1,
+                ShortCode = "TEST",
+                Description = "For testing purposes.",
+                DisplayName = "Test Account",
+                StartDate = DateTime.UtcNow.Date,
+                Attributes = new List<AccountBaseAttributeDto>()
+                {
+                    new()
+                    {
+                        AccountObjectId = 1,
+                        AttributeMember = new()
+                        {
+                            AttributeMemberId = 1,
+                            DisplayName = "Member 1",
+                            DisplayOrder = 1,
+                            Attribute = new()
+                            {
+                                AttributeId = 1,
+                                DisplayName = "Attribute 1"
+                            }
+                        },
+                        EffectiveDate = DateTime.UtcNow.Date,
+                        PercentWeight = 0.5M
+                    },
+                    testAttributeDto
+                }
+            };
+            var initialCountMinusOne = accountDto.Attributes.Count - 1;
 
             var mapper = new Mapper(new MapperConfiguration(x =>
             {
@@ -319,6 +401,7 @@ namespace NjordinSight.Test.DataTransfer.Mapping
             }));
 
             // Act
+            accountDto.AttributeCollection.EntryCollection.First().RemoveEntry(testAttributeDto);
             var account = mapper.Map<Account>(accountDto);
 
             // Assert
@@ -330,32 +413,99 @@ namespace NjordinSight.Test.DataTransfer.Mapping
                 accountDto.Attributes.Count,
                 account.AccountNavigation.AccountAttributeMemberEntries.Count);
 
-            // Fact: All property values match.
-            var comparisons = new List<Task>()
-            {
-                Task.Run(() => Assert.AreEqual(accountDto.Id, account.AccountNavigation.AccountObjectId)),
-                Task.Run(() => Assert.AreEqual(accountDto.ShortCode, account.AccountNavigation.AccountObjectCode)),
-                Task.Run(() => Assert.AreEqual(accountDto.StartDate, account.AccountNavigation.StartDate)),
-                Task.Run(() => Assert.AreEqual(accountDto.CloseDate, account.AccountNavigation.CloseDate)),
-                Task.Run(() => Assert.AreEqual(accountDto.DisplayName, account.AccountNavigation.ObjectDisplayName)),
-                Task.Run(() => Assert.AreEqual(accountDto.Description, account.AccountNavigation.ObjectDescription)),
-                Task.Run(() => Assert.AreEqual(accountDto.ObjectType, account.AccountNavigation.ObjectType)),
-                Task.Run(() => Assert.AreEqual(accountDto.AccountCustodianId, account.AccountCustodianId)),
-                Task.Run(() => Assert.AreEqual(accountDto.AccountNumber, account.AccountNumber)),
-                Task.Run(() => Assert.AreEqual(accountDto.HasWallet, account.HasWallet)),
-                Task.Run(() => Assert.AreEqual(accountDto.HasBankTransaction, account.HasBankTransaction)),
-                Task.Run(() => Assert.AreEqual(accountDto.HasBrokerTransaction, account.HasBrokerTransaction))
-            };
-
-            Task assertions = Task.WhenAll(comparisons);
-            await assertions;
-
-            if (assertions.Status == TaskStatus.Faulted)
-            {
-                Assert.Fail();
-            }
+            Assert.AreEqual(
+                expected: initialCountMinusOne,
+                actual: account.AccountNavigation.AccountAttributeMemberEntries.Count);
         }
 
-        
+        [TestMethod]
+        public void Account_MapFrom_AccountDto_WithModifiedAttribute_YieldsModifiedAttributeEntry()
+        {
+            // Arrange
+            var accountDto = new AccountDto()
+            {
+                Id = 1,
+                AccountNumber = "####-####-####",
+                HasBankTransaction = true,
+                HasBrokerTransaction = true,
+                AccountCustodianId = 1,
+                ShortCode = "TEST",
+                Description = "For testing purposes.",
+                DisplayName = "Test Account",
+                StartDate = DateTime.UtcNow.Date,
+                Attributes = new List<AccountBaseAttributeDto>()
+                {
+                    new()
+                    {
+                        AccountObjectId = 1,
+                        AttributeMember = new()
+                        {
+                            AttributeMemberId = 1,
+                            DisplayName = "Member 1",
+                            DisplayOrder = 1,
+                            Attribute = new()
+                            {
+                                AttributeId = 1,
+                                DisplayName = "Attribute 1"
+                            }
+                        },
+                        EffectiveDate = DateTime.UtcNow.Date,
+                        PercentWeight = 0.5M
+                    },
+                    new()
+                    {
+                        AccountObjectId = 1,
+                        AttributeMember = new()
+                        {
+                            AttributeMemberId = 2,
+                            DisplayName = "Member 2",
+                            DisplayOrder = 2,
+                            Attribute = new()
+                            {
+                                AttributeId = 1,
+                                DisplayName = "Attribute 1"
+                            }
+                        },
+                        EffectiveDate = DateTime.UtcNow.Date,
+                        PercentWeight = 0.5M
+                    }
+                }
+            };
+            
+            var mapper = new Mapper(new MapperConfiguration(x =>
+            {
+                x.AddProfile<ModelAttributeProfile>();
+                x.AddProfile<AccountProfile>();
+            }));
+
+            // Act
+            accountDto.AttributeCollection.CurrentEntries.First().AttributeMember = new()
+            {
+                AttributeMemberId = 3,
+                DisplayName = "Member 3",
+                DisplayOrder = 2,
+                Attribute = new()
+                {
+                    AttributeId = 1,
+                    DisplayName = "Attribute 1"
+                }
+            };
+
+            var account = mapper.Map<Account>(accountDto);
+
+            // Assert
+            // Fact: Instance is created.
+            Assert.IsInstanceOfType(account, typeof(Account));
+
+            // Fact: Attributes property is non-empty collection with count matching source 
+            //       and expected count from the arrangment.
+            Assert.AreEqual(
+                accountDto.Attributes.Count,
+                account.AccountNavigation.AccountAttributeMemberEntries.Count);
+
+            Assert.IsTrue(account.AccountNavigation
+                .AccountAttributeMemberEntries
+                .Any(x => x.AttributeMemberId == 3));
+        }
     }
 }
