4d2c7305f747de15ea080b4a67eaf7443f4bdf6d
hoeyi <48498580+hoeyi@users.noreply.github.com>
2024-06-09 18:03:27 -0400

    build: update docker compose to include installation scripts Add script for initializing the application databases. Update docker override to remove external port map for db container.

--- docker-compose-build.sh
+++ docker-compose-build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Prompt the user for values to assign to $MSSQL_SA_PASSWORD and $OZYM_APP_PASSWORD variables.
+read -s -p "Enter the password for the 'sa' SQL server user: " MSSQL_SA_PASSWORD
+echo
+read -s -p "Enter the password for the 'OzymAppUser' SQL server user: " OZYM_APP_PASSWORD
+
 # Get the last commit hash and pass it to the docker-compose build command
 export COMMIT_TAG=$(git log -1 --format=%h)
 docker compose build --build-arg COMMIT_TAG=$COMMIT_TAG
