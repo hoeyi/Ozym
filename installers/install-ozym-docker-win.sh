2e9d085077704d97e822f88ac7029ad59289a0d4
hoeyi <48498580+hoeyi@users.noreply.github.com>
2024-06-16 15:26:35 -0400

    repo: add web service restart and minor refactoring of installation script

--- installers/install-ozym-docker-win.sh
+++ installers/install-ozym-docker-win.sh
@@ -26,9 +26,9 @@ if [ "$CONTINUE_INSTALL" = "N" ]; then
     exit
 else
     echo -e "\nStarting installation...\n"
-    read -p 'Enter password for SQL [sa] user: ' -s MSSQL_SA_PASSWORD
+    read -p 'Enter password for SQL [sa] login: ' -s MSSQL_SA_PASSWORD
     echo
-    read -p 'Enter password for Ozym web app user: ' -s OZYM_APP_PASSWORD
+    read -p 'Enter password for SQL [OzymAppUser] login: ' -s OZYM_APP_PASSWORD
     echo 
 
     export MSSQL_SA_PASSWORD="$MSSQL_SA_PASSWORD"
@@ -37,13 +37,12 @@ else
     echo 'Starting Docker network...'
     docker compose up -d
 
+    echo 'Starting SQL Server...'
+    # Wait for the SQL engine to fully initialize to avoid connection error.
     sleep 10
 
     echo 'Setting up initial database login/user configuration...'
-    read -p 'Press any key to continue... '
     winpty docker exec ozym-db //opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -i init-ozym-db.sql -v OZYM_APP_PASSWORD="$OZYM_APP_PASSWORD"
-
-    read -p 'Press any key to continue... '
 
     echo 'Configurating sample database and applying data migrations...'
     winpty docker exec ozym-web //app/ozymworksbundle \
@@ -53,10 +52,12 @@ else
     winpty docker exec ozym-web //app/ozymidentitybundle \
         --connection "Server=ozymdb;Database=OzymIdentity;User Id=sa;Password=$MSSQL_SA_PASSWORD;TrustServerCertificate=true"
 
+    echo 'Restarting ozym-web container...'
+    docker restart ozym-web
+    
     # Clean up variables.
     unset MSSQL_SA_PASSWORD
     unset OZYM_APP_PASSWORD
-    unset init_sql_script
 
     echo -e '\nInitialization complete!'
 fi
