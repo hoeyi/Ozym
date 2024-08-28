c63b6c380a2b9c4cbc4be6fba1c92b82bd52077e
hoeyi <48498580+hoeyi@users.noreply.github.com>
2024-08-28 19:40:35 -0400

    refactor: move OnModelCreating to non-auto partial IdentityDbContext

--- Ozym.Web/Identity/Data/IdentityDbContext.cs
+++ Ozym.Web/Identity/Data/IdentityDbContext.cs
@@ -4,22 +4,7 @@ using Ozym.Web.Identity.Data;
 
 namespace Ozym.Web.Identity.Data;
 
-
-
-public partial class IdentityDbContext : IdentityDbContext<ApplicationUser>
+public partial class IdentityDbContext(
+    DbContextOptions<IdentityDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
-    public IdentityDbContext(DbContextOptions<IdentityDbContext> options)
-        : base(options)
-    {
-    }
-
-    protected override void OnModelCreating(ModelBuilder builder)
-    {
-        base.OnModelCreating(builder);
-        // Customize the ASP.NET Identity model and override the defaults if needed.
-        // For example, you can rename the ASP.NET Identity table names and more.
-        // Add your customizations after calling base.OnModelCreating(builder);
-
-        builder.HasDefaultSchema("WebIdentity");
-    }
 }
--- Ozym.Web/Identity/Data/IdentityDbContextExt.cs
+++ Ozym.Web/Identity/Data/IdentityDbContextExt.cs
@@ -16,5 +16,17 @@ namespace Ozym.Web.Identity.Data
                 optionsBuilder.UseInMemoryDatabase("OzymIdentity");
             }
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+
+            builder.HasDefaultSchema("WebIdentity");
+
+            builder.SeedData();
+        }
     }
 }
