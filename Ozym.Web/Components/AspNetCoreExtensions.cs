b30f4c31ba9d51cfb1dc2f67420ec2118dbb3cf4
hoeyi <48498580+hoeyi@users.noreply.github.com>
2024-08-31 18:15:46 -0400

    refactor: add extension method for comparing to an array of paths

---
+++ Ozym.Web/Components/AspNetCoreExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Ozym.Web.Components
+{
+    internal static class AspNetCoreExtensions
+    {
+        /// <summary>
+        /// Checks if the given path starts with any of the specified segments.
+        /// </summary>
+        /// <param name="path">The path to check.</param>
+        /// <param name="others">The segments to compare against.</param>
+        /// <returns><c>true</c> if the path starts with any of the specified segments; otherwise, <c>false</c>.</returns>
+        /// <remarks>Only the first match is confirmed. Subsequent matches are not checked for.</remarks>
+        public static bool StartsWithSegments(this PathString path, PathString[] others)
+        {
+            if (others.Length == 0)
+                throw new InvalidOperationException($"Paramater '{nameof(others)} must not be empty.");
+
+            bool result = false;
+            foreach (var other in others)
+            {
+                result = path.StartsWithSegments(other);
+                if (result) break;
+            }
+
+            return result;
+        }
+    }
+}
